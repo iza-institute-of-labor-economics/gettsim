77c4b4c4f132068889e7e204c3ab45423da754c8
MaxBlesch <s6mables@uni-bonn.de>
2019-06-15 00:07:02 +0200

    Splitted pensions functions. Changed function to work for a series not a DataFrame.
    Former-commit-id: b106f3c5219b6d65cc454c00560920bf7f69088d
    Former-commit-id: 85870710fdca42e0119ac414dd3234f78b6f1267

--- src/analysis/tax_transfer_funcs/pensions.py
+++ src/analysis/tax_transfer_funcs/pensions.py
@@ -1,11 +1,10 @@
 import numpy as np
-import pandas as pd
 
 # from termcolor import cprint
 
 
 def _ep_for_earnings(df, tb, tb_pens):
-    if df["east"].iloc[0]:
+    if df["east"]:
         westost = "o"
     else:
         westost = "w"
@@ -39,44 +38,20 @@ def update_earnings_points(df, tb, tb_pens):
     return df["EP"] + out
 
 
-def pensions(df, tb, tb_pens):
-    """ Old-Age Pensions
-
-    models 'Rentenformel':
-    https://de.wikipedia.org/wiki/Rentenformel
-    https://de.wikipedia.org/wiki/Rentenanpassungsformel
-
-    - In particular, it calculates the "entgeltpunkte" for the previous year, based on
-      earnings of that year. These need to be related to average earnings
-    - As we do not know previously collect Entgeltpunkte, we take an average
-      value (to be improved)
-
-    """
-    r = pd.DataFrame(index=df.index)
-    # cprint("Pensions", "red", "on_white")
-    # meanwages is only filled until 2016
-    yr = df["year"].iloc[0]
-
-    # individuelle monatl. Altersrente (Rentenartfaktor = 1):
-    # R = EP * ZF * Rw
-
-    # Add values for current year: ratio of own wage (up to the threshold)
-    # to the mean wage
-    r["EP"] = update_earnings_points(df, tb, tb_pens[yr])
-    # ZF: Zugangsfaktor. Depends on the age of entering pensions.
-    # For each year entering earlier (later) than the statutory retirement age,
-    # you get a penalty (reward) of 3.6 pp.
-    r["regelaltersgrenze"] = 65
+def calc_regelaltersgrenze(df_row):
     # If born after 1947, each birth year raises the age threshold by one month.
-    r.loc[df["byear"] > 1947, "regelaltersgrenze"] = np.minimum(
-        67, ((df["byear"] - 1947) / 12) + 65
-    )
-    r["ZF"] = ((df["age"] - r["regelaltersgrenze"]) * 0.036) + 1
+    if df_row["byear"] > 1947:
+        return np.minimum(67, ((df_row["byear"] - 1947) / 12) + 65)
+    else:
+        return 65
 
+
+def calc_rentenwert_from_2018(tb_pens, yr):
     # Rentenwert: The monetary value of one 'entgeltpunkt'.
     # This depends, among others, of past developments.
     # Hence, some calculations have been made
     # in the data preparation.
+    # First the Lohnkomponente which depands on the wage development of last years.
     lohnkomponente = tb_pens.loc["meanwages", yr - 1] / (
         tb_pens.loc["meanwages", yr - 2]
         * (
@@ -87,7 +62,6 @@ def pensions(df, tb, tb_pens):
             )
         )
     )
-
     riesterfaktor = (
         100 - tb_pens.loc["ava", yr - 1] - tb_pens.loc["rvbeitrag", yr - 1]
     ) / (100 - tb_pens.loc["ava", yr - 2] - tb_pens.loc["rvbeitrag", yr - 2])
@@ -100,23 +74,58 @@ def pensions(df, tb, tb_pens):
         )
     # Nachhaltigskeitsfaktor
     nachhfaktor = 1 + ((1 - (rq[1] / rq[2])) * tb_pens.loc["alpha", yr])
-    # use external (SOEP) or internal value for rentenwert?
-    if yr <= 2017:
-        rentenwert = tb_pens.loc["rentenwert_ext", yr]
-    else:
-        # Rentenwert must not be lower than in the previous year.
-        rentenwert = tb_pens.loc["rentenwert_ext", yr - 1] * min(
-            1, lohnkomponente * riesterfaktor * nachhfaktor
-        )
-        print(
-            "Änderung des Rentenwerts: "
-            + str(min(1, lohnkomponente * riesterfaktor * nachhfaktor))
-        )
+    # Rentenwert must not be lower than in the previous year.
+    rentenwert = tb_pens.loc["rentenwert_ext", yr - 1] * min(
+        1, lohnkomponente * riesterfaktor * nachhfaktor
+    )
+    print(
+        "Änderung des Rentenwerts: "
+        + str(min(1, lohnkomponente * riesterfaktor * nachhfaktor))
+    )
+    return rentenwert
+
+
+def calc_rentenwert_until_2017(tb_pens, yr):
+    return tb_pens.loc["rentenwert_ext", yr]
+
+
+def pensions(df_row, tb, tb_pens):
+    """
+    This function calculates the Old-Age Pensions claim if the agent chooses to
+    retire. The function basicly follows the following equation
+    # R = EP * ZF * Rw
+
+    models 'Rentenformel':
+    https://de.wikipedia.org/wiki/Rentenformel
+    https://de.wikipedia.org/wiki/Rentenanpassungsformel
+
+    - In particular, it calculates the "entgeltpunkte" for the previous year, based on
+      earnings of that year. These need to be related to average earnings
+    - As we do not know previously collect Entgeltpunkte, we take an average
+      value (to be improved)
+
+    """
+    # cprint("Pensions", "red", "on_white")
+    # meanwages is only filled until 2016
+    yr = min(df_row["year"], 2016)
+
+    EP = update_earnings_points(df_row, tb, tb_pens[yr])
+    # ZF: Zugangsfaktor. Depends on the age of entering pensions.
+    # At the regelaltersgrenze, the agent is allowed to get pensions with his full
+    # claim.
+    regelaltersgrenze = calc_regelaltersgrenze(df_row)
+
+    ZF = ((df_row["age"] - regelaltersgrenze) * 0.036) + 1
+
+    # individuelle monatl. Altersrente (Rentenartfaktor = 1):
+    # Calculate rentenwert either using external (SOEP) or internal value for
+    # rentenwert.
+    rentenwert = tb["calc_rentenwert"](tb_pens, yr)
 
     # use all three components for Rentenformel.
     # There is an additional 'Rentenartfaktor', equal to 1 for old-age pensions.
     # It's called 'pensions_sim' to emphasize that this is simulated.
 
-    r["pensions_sim"] = np.maximum(0, round(r["EP"] * r["ZF"] * rentenwert, 2))
+    pensions_sim = np.maximum(0, round(EP * ZF * rentenwert, 2))
 
-    return r["pensions_sim"]
+    return pensions_sim
--- src/analysis/tax_transfer_funcs/test_tax_transfers/test_pensions.py
+++ src/analysis/tax_transfer_funcs/test_tax_transfers/test_pensions.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import numpy as np
 import pytest
-from pandas.testing import assert_series_equal
-from src.analysis.tax_transfer_funcs.pensions import pensions, update_earnings_points
+from numpy.testing import assert_array_almost_equal
+from src.analysis.tax_transfer_funcs.pensions import (
+    pensions,
+    update_earnings_points,
+    calc_rentenwert_until_2017,
+    calc_rentenwert_from_2018,
+)
 from bld.project_paths import project_paths_join as ppj
 
 from src.analysis.tax_transfer_funcs.test_tax_transfers.auxiliary_test_tax import (
@@ -36,12 +42,18 @@ def test_pension(year):
     column = "pensions_sim"
     df = load_input(year, "test_dfs_pensions.xlsx", input_cols)
     tb = load_tb(year)
+    if year > 2017:
+        tb["calc_rentenwert"] = calc_rentenwert_from_2018
+    else:
+        tb["calc_rentenwert"] = calc_rentenwert_until_2017
     tb_pens = pd.read_excel(ppj("IN_DATA", "pensions.xlsx")).set_index("var")
     expected = load_output(year, "test_dfs_pensions.xlsx", column)
     calculated = pd.Series(name=column)
     for pid in df["pid"].unique():
-        calculated = calculated.append(pensions(df[df["pid"] == pid], tb, tb_pens))
-    assert_series_equal(calculated, expected)
+        calculated = np.append(
+            calculated, pensions(df[df["pid"] == pid].iloc[0], tb, tb_pens)
+        )
+    assert_array_almost_equal(calculated, expected)
 
 
 @pytest.mark.parametrize("year", years)
@@ -51,9 +63,10 @@ def test_update_earning_points(year):
     tb = load_tb(year)
     tb_pens = pd.read_excel(ppj("IN_DATA", "pensions.xlsx")).set_index("var")
     expected = load_output(year, "test_dfs_pensions.xlsx", column)
-    calculated = pd.Series(name=column)
+    calculated = np.array([])
     for pid in df["pid"].unique():
-        calculated = calculated.append(
-            update_earnings_points(df[df["pid"] == pid], tb, tb_pens[year])
+        calculated = np.append(
+            calculated,
+            update_earnings_points(df[df["pid"] == pid].iloc[0], tb, tb_pens[year]),
         )
-    assert_series_equal(calculated, expected, check_names=False)
+    assert_array_almost_equal(calculated, expected.values)
