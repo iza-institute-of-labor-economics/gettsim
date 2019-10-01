9093d140f8c49000741cac7b00bd0d25ab7f0025
Tobias Raabe <tobiasraabe@uni-bonn.de>
2019-10-01 20:33:16 +0200

    Editable install of package.

--- gettsim/tests/test_data/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This module contains test data.
-
-Short description
-- All columns up to the green one provide the input for a certain function.
-- The green column contains the expected result.
-- All columns on the righthandside of the green column contain helpful calculations.
-
-"""
