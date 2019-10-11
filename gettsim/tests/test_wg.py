8a279467c621732b2be666f892ab9af4adf64db2
Eric Sommer <ericsomm@gmx.de>
2019-10-11 11:38:46 +0200

    incorporate yet another wg test from master

--- gettsim/tests/test_wg.py
+++ gettsim/tests/test_wg.py
@@ -61,3 +61,21 @@ def test_wg(year):
         calculated = calculated.append(wg(df[df["hid"] == hid], tb)[columns])
     expected = load_test_data(year, file_name, columns)
     assert_frame_equal(calculated, expected)
+
+
+@pytest.mark.parametrize("year", [2013])
+def test_wg_no_mietstufe_in_input_data(year):
+    file_name = "test_dfs_wg2.csv"
+    columns = ["wohngeld_basis_hh"]
+    df = load_test_data(year, file_name, input_cols)
+    tb = load_tb(year)
+    tb["yr"] = year
+    if year < 2009:
+        tb["calc_max_rent"] = calc_max_rent_until_2008
+    else:
+        tb["calc_max_rent"] = calc_max_rent_since_2009
+    calculated = pd.DataFrame(columns=columns)
+    for hid in df["hid"].unique():
+        calculated = calculated.append(wg(df[df["hid"] == hid], tb)[columns])
+    expected = load_test_data(year, file_name, columns)
+    assert_frame_equal(calculated, expected, check_exact=False, check_less_precise=2)
