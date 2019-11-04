0bba3fa9f220542a3ee7da1f54e6e68d58aa3c12
MaxBlesch <s6mables@uni-bonn.de>
2019-11-05 00:44:07 +0100

    Deleted list and keys() from policy date function.

--- gettsim/tests/policy_for_date.py
+++ gettsim/tests/policy_for_date.py
@@ -8,7 +8,7 @@ def get_policies_for_date(tb_pre, year, month=1, day=1):
     this_year = datetime.date(year=year, month=month, day=day)
     for key in tb_pre.keys():
         if tb_pre[key]["values"] is not None:
-            policy_dates = list(tb_pre[key]["values"].keys())
+            policy_dates = tb_pre[key]["values"]
             past_policies = [x for x in policy_dates if x <= this_year]
             if not past_policies:
                 # TODO: Should there be missing values or should the key not exist?
