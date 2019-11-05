9492f419fef66cb1f019180eba111a4ccce6cd37
Tobias Raabe <tobiasraabe@uni-bonn.de>
2019-11-05 23:46:41 +0100

    testing command is more conscise.

--- gettsim/__init__.py
+++ gettsim/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 from gettsim.config import ROOT_DIR
@@ -9,7 +7,4 @@ __version__ = "0.2.0"
 
 
 def test():
-    cwd = os.getcwd()
-    os.chdir(ROOT_DIR)
-    pytest.main()
-    os.chdir(cwd)
+    pytest.main([str(ROOT_DIR)])
