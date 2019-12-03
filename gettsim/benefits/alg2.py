4251724391f11389ad37bddaac34389930b91be0
Maximilian Blömer <bloemer@econ.lmu.de>
2019-12-03 14:46:26 +0100

    Fix interval 2 threshold

--- gettsim/benefits/alg2.py
+++ gettsim/benefits/alg2.py
@@ -334,7 +334,7 @@ def e_anr_frei_person_2005_10(person, params, a2eg3):
             m_wage - params["a2grf"]
         )
 
-    elif params["a2eg1"] <= m_wage < a2eg3:
+    elif params["a2eg2"] <= m_wage < a2eg3:
         person["ekanrefrei"] = (
             params["a2an1"] * params["a2eg1"]
             + params["a2an2"] * (params["a2eg2"] - params["a2eg1"])
