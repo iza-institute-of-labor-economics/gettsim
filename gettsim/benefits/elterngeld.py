849270e39f4e0ef9880e7afa078457c04e059c71
MaxBlesch <s6mables@uni-bonn.de>
2019-12-04 12:14:34 +0100

    Moved to csv. Changed to person.

--- gettsim/benefits/elterngeld.py
+++ gettsim/benefits/elterngeld.py
@@ -1,35 +1,13 @@
-from gettsim.tests.auxiliary_test_tax import load_tb
-
-def elt_geld(df):
+def elt_geld(person, params):
     """This function calculates the monthly benefits for having
     a child that is up to one year old (Elterngeld)"""
 
-    eltg_df = df
+    if person["elt_zeit"]:
+        elt_geld_calc = person["m_wage"] * params["elgfaktor"]
 
-    eltg_df["elt_geld"] = 0
+        person["elt_geld"] = min(elt_geld_calc, params["elgmax"])
 
-    receiving = eltg_df.loc[eltg_df["elt_zeit"], "pid"]
+    else:
+        person["elt_geld"] = 0
 
-    elt_geld = []
-
-    for pid in receiving:
-        referece_year = (
-            eltg_df.loc[(eltg_df["elt_zeit"] == True) & (eltg_df["pid"] == pid), "year"]
-            - 1
-        )
-
-        tb = load_tb(referece_year.item())
-
-        calculated = (
-            eltg_df.loc[
-                (eltg_df["pid"] == pid) & (eltg_df["year"] == referece_year.item()),
-                "m_wage",
-            ].item()
-            * tb["elgfaktor"]
-        )
-
-        elt_geld += [min(calculated, tb["elgmax"])]
-
-    eltg_df.loc[eltg_df["elt_zeit"], "elt_geld"] = elt_geld
-
-    return eltg_df["elt_geld"]
+    return person
