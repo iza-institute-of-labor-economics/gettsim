7185d420ae870a8e4afdcee0bf1fd58021dcf496
Eric Sommer <ericsomm@gmx.de>
2019-12-13 17:29:29 +0100

    reform actually implemented in 07/2017

--- gettsim/benefits/unterhaltsvorschuss.py
+++ gettsim/benefits/unterhaltsvorschuss.py
@@ -16,16 +16,11 @@ def uhv(tax_unit, params, kindergeld_params):
     tax_unit: Updated DataFrame including uhv
     """
 
-    # Before 2017, UHV was only paid up to 6 years, which is why we model it only since then.
-    if params["year"] >= 2017:
-        return uhv_since_2017(tax_unit, params, kindergeld_params)
-    else:
-        tax_unit["uhv"] = 0
-        return tax_unit
+    return params["uhv_calc"](tax_unit, params, kindergeld_params)
 
 
-def uhv_since_2017(tax_unit, params, kindergeld_params):
-    """ UHV ruling since 2017. Before 2017, basically the same, but
+def uhv_since_07_2017(tax_unit, params, kindergeld_params):
+    """ UHV ruling since 07/2017. Before 2017, basically the same, but
         eligibility was more restrictive.
 
         returns:
@@ -35,12 +30,12 @@ def uhv_since_2017(tax_unit, params, kindergeld_params):
     # and the child benefit for the first child.
     tax_unit["uhv"] = 0
     # Amounts depend on age
-    tax_unit.loc[tax_unit["age"].between(0, 6) & tax_unit["alleinerz"], "uhv"] = (
+    tax_unit.loc[(tax_unit["age"] <= 6) & tax_unit["alleinerz"], "uhv"] = (
         params["uhv6_amount"] - kindergeld_params["kgeld1"]
     )
-    tax_unit.loc[tax_unit["age"].between(7, 12) & tax_unit["alleinerz"], "uhv"] = (
-        params["uhv12_amount"] - kindergeld_params["kgeld1"]
-    )
+    tax_unit.loc[
+        (tax_unit["age"] >= 7) & (tax_unit["age"] <= 12) & tax_unit["alleinerz"], "uhv"
+    ] = (params["uhv12_amount"] - kindergeld_params["kgeld1"])
     # Older kids get it only if the parent has income > 600€
     uhv_inc_tu = (
         tax_unit[
@@ -58,7 +53,8 @@ def uhv_since_2017(tax_unit, params, kindergeld_params):
         .sum()
     )
     tax_unit.loc[
-        (tax_unit["age"].between(13, 17))
+        (tax_unit["age"] >= 13)
+        & (tax_unit["age"] <= 17)
         & (tax_unit["alleinerz"])
         & (uhv_inc_tu > params["uhv17_inc"]),
         "uhv",
@@ -68,4 +64,12 @@ def uhv_since_2017(tax_unit, params, kindergeld_params):
     tax_unit["uhv"] = np.ceil(tax_unit["uhv"]).astype(int)
 
     # TODO: Check against actual transfers
+    return tax_unit
+
+
+def uhv_pre_07_2017(tax_unit, params, kindergeld_params):
+    """
+    UHV before 07/2017. Not implemented yet, as it was only paid for 6 years.
+    """
+    tax_unit["uhv"] = 0
     return tax_unit
