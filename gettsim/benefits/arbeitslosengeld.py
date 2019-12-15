a3405059c019ccec1e60cf878965725fa6e4f39b
MaxBlesch <s6mables@uni-bonn.de>
2019-12-15 17:06:13 +0100

    Renamed pausch soz_vers for alg1.

--- gettsim/benefits/arbeitslosengeld.py
+++ gettsim/benefits/arbeitslosengeld.py
@@ -50,7 +50,7 @@ def proxy_net_wage_last_year(
     max_wage = min(soz_vers_beitr_params[f"rvmaxek{westost}"], person["m_wage_l1"])
 
     # We need to deduct lump-sum amounts for contributions, taxes and soli
-    prox_ssc = arbeitsl_geld_params["alg1_abz"] * max_wage
+    prox_ssc = arbeitsl_geld_params["soz_vers_pausch"] * max_wage
 
     # Fictive taxes (Lohnsteuer) are approximated by applying the wage to the tax tariff
     prox_tax = e_st_params["tax_schedule"](
