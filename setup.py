75f9575657e5aff1c9bd8943bc2f0e123a9aa0fe
Tobias Raabe <tobiasraabe@uni-bonn.de>
2020-03-04 14:29:44 +0100

    Renamed some config files and put all conda files into .conda.

--- setup.py
+++ setup.py
@@ -39,9 +39,7 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     platforms="any",
-    package_data={
-        "gettsim": ["tests/test_data/*.csv", "data/param.yaml", "data/*.xlsx"]
-    },
+    package_data={"gettsim": ["tests/test_data/*.csv", "data/*.yaml", "data/*.xlsx"]},
     include_package_data=True,
     zip_safe=False,
 )
