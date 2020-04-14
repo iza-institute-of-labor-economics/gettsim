9ecc0d34391b7200efff7bb0e93d912de7f1a941
MaxBlesch <s6mables@uni-bonn.de>
2020-04-14 16:15:51 +0200

    Changed loading function. Deleted old file.

--- gettsim/tests/conftest.py
+++ gettsim/tests/conftest.py
@@ -16,7 +16,8 @@ def eink_st_raw_data():
 
 @pytest.fixture(scope="session")
 def eink_st_abzuege_raw_data():
-    return yaml.safe_load((ROOT_DIR / "data" / "eink_st_abzuege.yaml").read_text())
+    with open(ROOT_DIR / "data" / "eink_st_abzuege.yaml", encoding="utf8") as yml:
+        return yaml.safe_load(yml)
 
 
 @pytest.fixture(scope="session")
