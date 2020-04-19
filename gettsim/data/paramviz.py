7db10e007431c5d50db98a109481ef3fe3e130bb
Eric Sommer <ericsomm@gmx.de>
2020-04-19 22:25:17 +0200

    more steps

--- gettsim/data/paramviz.py
+++ gettsim/data/paramviz.py
@@ -24,6 +24,13 @@ def plotstyle(p):
     return p
 
 
+def reshape_grouped_param(s):
+    """ Reshape grouped dataframe by unpacking the value column"""
+
+    for r in range(len(s)):
+        pass
+
+
 def parse_yaml(params, key, lang, grouped=False):
     # Legal name of parameter
     name = params[key]["name"][lang]
@@ -42,6 +49,7 @@ def parse_yaml(params, key, lang, grouped=False):
         # Grouped Parameters
         all_dates = [key for key in params[key].keys() if type(key) == datetime.date]
         for d in all_dates:
+            # is it a value or something more complicated?
             if "scalar" in params[key][d].keys():
                 row = {
                     "date": d,
@@ -49,16 +57,23 @@ def parse_yaml(params, key, lang, grouped=False):
                     "note": params[key][d]["reference"],
                 }
             else:
-                row = {
-                    "date": d,
-                    "value": params[key][d],
-                    "note": params[key][d]["reference"],
-                }
+                row = {"date": d}
+                if "reference" in params[key].keys():
+                    row["note"] = params[key][d]["reference"]
+                else:
+                    row["note"] = None
+                # value as dictionary. use the bit from policy_for_date.py
+
             par = par.append(row, ignore_index=True)
 
     # Some cleaning
     par["date"] = pd.to_datetime(par["date"], format="%Y-%m-%d")
-    par["value"] = par["value"].astype(float)
+    par.to_csv('~/test/par.csv', index=False)
+    try:
+        par["value"] = par["value"].astype(float)
+    except TypeError:
+        par["value"] = reshape_grouped_param(par["value"])
+
     par["note"] = par["note"].fillna("")
     if par["value"].max() < 1:
         unit = "share"
@@ -74,10 +89,12 @@ def make_param_graphs(lang="de"):
     # get list of params
     param_files = glob.glob("*.yaml")
 
-    for yaml_file in param_files:
-        # for yaml_file in ["wohngeld.yaml"]:
+    #for yaml_file in param_files:
+    for yaml_file in ["wohngeld.yaml"]:
+        print(f"reading {yaml_file}...")
         file_name = yaml_file[:-5]
         output_file(f"graphs/{file_name}.html", title=file_name.title())
+        grouped_params = ["wohngeld", "kinderzuschlag"]
 
         # Read YAML
         with open(f"{yaml_file}") as f:
@@ -86,11 +103,12 @@ def make_param_graphs(lang="de"):
         all_keys = params.keys()
         plotlist = []
         for key in all_keys:
-            if "wohngeld" not in yaml_file:
+            if yaml_file[:-5] in grouped_params:
+                name, descr, par, unit = parse_yaml(params, key, lang, True)
+            else:
                 # The DataFrame 'par' contains the data we want to plot
                 name, descr, par, unit = parse_yaml(params, key, lang)
-            else:
-                name, descr, par, unit = parse_yaml(params, key, lang, True)
+
             # How to format the values on mouse-over
             if unit == "share":
                 format_str = "$y{0.3f}"
@@ -145,7 +163,7 @@ def make_param_graphs(lang="de"):
         grid = gridplot(plotlist, ncols=3)
 
         save(grid)
-        print(f"File {file_name}.html created.")
+        print(f"Parameter Visualization: File {file_name}.html created.")
 
 
 make_param_graphs()
