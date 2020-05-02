e308ab0319e66e551777638f9f1f95973a29db6f
MaxBlesch <s6mables@uni-bonn.de>
2020-05-02 13:57:51 +0200

    Added key dropping.

--- gettsim/pre_processing/policy_for_date.py
+++ gettsim/pre_processing/policy_for_date.py
@@ -193,6 +193,9 @@ def process_data(policy_date, group, raw_group_data=None, parameters=None):
                         piecewise_polynomial,
                         func_type=tax_data[param]["type"].split("_")[1],
                     )
+            for key in ["type", "progressionsfaktor"]:
+                tax_data[param].pop(key, None)
+
     if group == "arbeitsl_geld_2":
         if tax_data["jahr"] <= 2010:
             tax_data["calc_regelsatz"] = regelberechnung_until_2010
