e794147681fca1e1158886382afd6f6827467ffa
Tobias Raabe <raabe@posteo.de>
2020-05-15 20:15:16 +0200

    Add test for naming conventions.

---
+++ gettsim/tests/test_gep_2_naming_conventions.py
@@ -0,0 +1,20 @@
+import pytest
+from gettsim.config import ROOT_DIR, GEP_2_CHARACTER_LIMIT
+from gettsim.functions_loader import load_functions
+
+
+@pytest.mark.xfail
+def test_length_of_variable_names():
+    sources = [
+        ROOT_DIR / "benefits",
+        ROOT_DIR / "soz_vers",
+        ROOT_DIR / "taxes",
+        ROOT_DIR / "renten_anspr.py",
+        ROOT_DIR / "verfügb_eink.py",
+    ]
+
+    functions = load_functions(sources)
+
+    over_limit = [name for name in functions if len(name) >= GEP_2_CHARACTER_LIMIT]
+
+    assert not over_limit
