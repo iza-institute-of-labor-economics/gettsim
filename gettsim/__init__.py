200c7c7d1ab4e4eb99599677e8b0ce0586f4080e
Tobias Raabe <raabe@posteo.de>
2020-05-28 13:40:09 +0200

    Start vis.

--- gettsim/__init__.py
+++ gettsim/__init__.py
@@ -1,6 +1,8 @@
 import pytest
 
 from gettsim.config import ROOT_DIR
+from gettsim.dag import compute_taxes_and_transfers  # noqa: F401
+from gettsim.pre_processing.policy_for_date import get_policies_for_date  # noqa: F401
 
 
 __version__ = "0.2.1"
