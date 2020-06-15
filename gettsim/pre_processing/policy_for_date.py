defb9a869520c4373646cf27649bf7c2d23edd2a
Maximilian Blesch <s6mables@uni-bonn.de>
2020-06-15 11:11:38 +0200

    Update policy for date (#196)
    * Updated docstrings in `policy_for_date.py`.
    *  Refactored code in `policy_for_date.py`.

--- gettsim/pre_processing/policy_for_date.py
+++ gettsim/pre_processing/policy_for_date.py
@@ -27,7 +27,9 @@ from gettsim.benefits.wohngeld import wohngeld_eink_abzüge_ab_2016
 from gettsim.benefits.wohngeld import wohngeld_eink_abzüge_bis_2015
 from gettsim.benefits.wohngeld import wohngeld_max_miete_ab_2009
 from gettsim.benefits.wohngeld import wohngeld_max_miete_bis_2008
+from gettsim.config import INTERNAL_PARAM_GROUPS
 from gettsim.config import ROOT_DIR
+from gettsim.interface import create_linewise_printed_list
 from gettsim.pre_processing.piecewise_functions import get_piecewise_parameters
 from gettsim.pre_processing.policy_completion_funcs import add_progressionsfaktor
 from gettsim.taxes.favorability_check import _eink_st_tu_ab_1997
@@ -48,62 +50,174 @@ from gettsim.taxes.zu_verst_eink.vorsorge import _vorsorge_ab_2010
 from gettsim.taxes.zu_verst_eink.vorsorge import _vorsorge_bis_2004
 
 
-def get_policies_for_date(policy_date, groups="all"):
-    if isinstance(policy_date, str):
-        policy_date = pd.to_datetime(policy_date)
+def get_policies_for_date(policy_date, policy_groups="all"):
+    """Get the state of the policy system for a particular date.
 
-    all_params_groups = [
-        "eink_st",
-        "eink_st_abzuege",
-        "soli_st",
-        "arbeitsl_geld_2",
-        "arbeitsl_geld",
-        "soz_vers_beitr",
-        "unterhalt",
-        "abgelt_st",
-        "wohngeld",
-        "kinderzuschlag",
-        "kindergeld",
-        "elterngeld",
-    ]
-    if type(groups) == list:
-        group_list = groups
-    elif type(groups) == str:
-        if groups == "all":
-            group_list = all_params_groups
-        elif groups in all_params_groups:
-            group_list = [groups]
-        else:
-            raise ValueError(f"{groups} is not a category for groups.")
-    else:
-        raise ValueError(f"{groups} is not a string or list.")
+    The function returns time dependent policy reforms (functions) as well as the
+    policy parameters for the current policy system.
+
+    Parameters
+    ----------
+    policy_date : int, str, datetime.date
+        The date for which the policy system is set up.
+    policy_groups : list, str
+        The group or a list of groups which parameters are loaded. If an invalid
+        name is given, a list of all possible values is printed. Default is to load all
+        parameter groups.
+
+    Returns
+    -------
+    params_dict : dict
+        Dictionary of parameters grouped in policy system compartments given in groups.
+    policy_func_dict : dict
+        Dictionary of time dependent policy reforms. Keys are the variable names they
+        create.
+
+    """
+    # Check policy date for correct format and transfer to datetime.date
+    policy_date = _parse_date(policy_date)
+
+    # Check groups argument for correct format and transfer to list.
+    group_list = _parse_parameter_groups(policy_groups)
 
     params_dict = {}
-    year = policy_date.year
 
     for group in group_list:
-        tax_data = load_data(policy_date, group)
+        tax_data = _load_parameter_group_from_yaml(policy_date, group)
 
-        for param in tax_data:
-            if type(tax_data[param]) == dict:
-                if "type" in tax_data[param]:
-                    if tax_data[param]["type"].startswith("piecewise"):
-                        if "progressionsfaktor" in tax_data[param]:
-                            if tax_data[param]["progressionsfaktor"]:
-                                tax_data[param] = add_progressionsfaktor(
-                                    tax_data[param], param
-                                )
-                        tax_data[param] = get_piecewise_parameters(
-                            tax_data[param],
-                            param,
-                            func_type=tax_data[param]["type"].split("_")[1],
-                        )
-                for key in ["type", "progressionsfaktor"]:
-                    tax_data[param].pop(key, None)
+        # Align paramters for e.g. piecewise polynomial functions
+        params_dict[group] = _parse_parameters(tax_data)
 
-        tax_data["datum"] = policy_date
-        params_dict[group] = tax_data
+    policy_func_dict = load_reforms_for_date(policy_date)
 
+    return params_dict, policy_func_dict
+
+
+def _parse_parameters(tax_data):
+    """Check if parameters are stored in implicit structures and align to general
+    structure.
+
+    Parameters
+    ----------
+    tax_data : dict
+        Loaded raw tax data.
+
+    Returns
+    -------
+    tax_data : dict
+        Parsed parameters ready to use in gettsim.
+
+    """
+    for param in tax_data:
+        if isinstance(tax_data[param], dict):
+            if "type" in tax_data[param]:
+                if tax_data[param]["type"].startswith("piecewise"):
+                    if "progressionsfaktor" in tax_data[param]:
+                        if tax_data[param]["progressionsfaktor"]:
+                            tax_data[param] = add_progressionsfaktor(
+                                tax_data[param], param
+                            )
+                    tax_data[param] = get_piecewise_parameters(
+                        tax_data[param],
+                        param,
+                        func_type=tax_data[param]["type"].split("_")[1],
+                    )
+            for key in ["type", "progressionsfaktor"]:
+                tax_data[param].pop(key, None)
+
+    return tax_data
+
+
+def _parse_parameter_groups(policy_groups):
+    """Check group argument for correct format and transfer to list.
+
+    Parameters
+    ----------
+    policy_groups : list, str
+        The group or a list of groups which parameters are loaded. Default is
+        all parameters
+
+    Returns
+    -------
+        List of groups to be loaded.
+
+    """
+    if isinstance(policy_groups, list):
+        misspelled = [
+            group for group in policy_groups if group not in INTERNAL_PARAM_GROUPS
+        ]
+        if not misspelled:
+            out = policy_groups
+        else:
+            part_1 = "The groups"
+            list_1 = create_linewise_printed_list(misspelled)
+            part_2 = "are not in the internal yaml files."
+            part_3 = "Possible group names are:"
+            list_formatted_internal_groups = create_linewise_printed_list(
+                INTERNAL_PARAM_GROUPS
+            )
+
+            raise ValueError(
+                "\n".join(
+                    [part_1, list_1, part_2, part_3, list_formatted_internal_groups]
+                )
+            )
+    elif isinstance(policy_groups, str):
+        if policy_groups == "all":
+            out = INTERNAL_PARAM_GROUPS
+        elif policy_groups in INTERNAL_PARAM_GROUPS:
+            out = [policy_groups]
+        else:
+            part_1 = f"{policy_groups} is not a valid group name."
+            part_2 = "Possible group names are:"
+            list_formatted_internal_groups = create_linewise_printed_list(
+                INTERNAL_PARAM_GROUPS
+            )
+            raise ValueError(
+                "\n".join([part_1, part_2, list_formatted_internal_groups])
+            )
+    else:
+        raise ValueError(f"{policy_groups} is not a string or list.")
+
+    return out
+
+
+def _parse_date(policy_date):
+    """Check the policy date for different input formats.
+
+    Parameters
+    ----------
+    policy_date : datetime.date, str, int
+        The date for which the policy system is set up.
+
+    Returns
+    -------
+    policy_date : datetime.date
+        The date for which the policy system is set up.
+    """
+    if isinstance(policy_date, str):
+        policy_date = pd.to_datetime(policy_date).date()
+    elif isinstance(policy_date, int):
+        policy_date = datetime.date(year=policy_date, month=1, day=1)
+    return policy_date
+
+
+def load_reforms_for_date(policy_date):
+    """Load time dependet policy reforms.
+
+    Parameters
+    ----------
+    policy_date : datetime.date
+        The date for which the policy system is set up.
+
+    Returns
+    -------
+    policy_func_dict : dict
+        Dictionary of time dependent policy reforms. Keys are the variable names they
+        create.
+
+    """
+    year = policy_date.year
     policy_func_dict = {}
     if year < 2009:
         policy_func_dict["sum_brutto_eink"] = _sum_brutto_eink_mit_kapital
@@ -179,14 +293,31 @@ def get_policies_for_date(policy_date, groups="all"):
     else:
         policy_func_dict["eink_anr_frei"] = eink_anr_frei_ab_10_2005
 
-    return params_dict, policy_func_dict
+    return policy_func_dict
 
 
-def load_data(policy_date, group, raw_group_data=None, parameters=None):
-    if not raw_group_data:
-        raw_group_data = yaml.safe_load(
-            (ROOT_DIR / "data" / f"{group}.yaml").read_text(encoding="utf-8")
-        )
+def _load_parameter_group_from_yaml(policy_date, group, parameters=None):
+    """Load data from raw yaml group file.
+
+    Parameters
+    ----------
+    policy_date : datetime.date
+        The date for which the policy system is set up.
+    group : string
+        Policy system compartment.
+    parameters : list
+        List of parameters to be loaded. Only relevant for in function calls.
+
+    Returns
+    -------
+    tax_data : dict
+        Dictionary of parameters loaded from raw yaml file and striped of
+        unnecessary keys.
+
+    """
+    raw_group_data = yaml.safe_load(
+        (ROOT_DIR / "data" / f"{group}.yaml").read_text(encoding="utf-8")
+    )
 
     # Keys from the raw file which will not be transferred
     not_trans_keys = ["note", "reference", "deviation_from"]
@@ -195,7 +326,9 @@ def load_data(policy_date, group, raw_group_data=None, parameters=None):
         parameters = raw_group_data.keys()
     for param in parameters:
         policy_dates = sorted(
-            key for key in raw_group_data[param].keys() if type(key) == datetime.date
+            key
+            for key in raw_group_data[param].keys()
+            if isinstance(key, datetime.date)
         )
 
         past_policies = [x for x in policy_dates if x <= policy_date]
@@ -207,11 +340,8 @@ def load_data(policy_date, group, raw_group_data=None, parameters=None):
                 future_policy = raw_group_data[param][np.min(policy_dates)]
                 if "." in future_policy["deviation_from"]:
                     path_list = future_policy["deviation_from"].split(".")
-                    tax_data[param] = load_data(
-                        policy_date,
-                        path_list[0],
-                        raw_group_data=raw_group_data,
-                        parameters=[path_list[1]],
+                    tax_data[param] = _load_parameter_group_from_yaml(
+                        policy_date, path_list[0], parameters=[path_list[1]],
                     )[path_list[1]]
             else:
                 # TODO: Should there be missing values or should the key not exist?
@@ -233,19 +363,13 @@ def load_data(policy_date, group, raw_group_data=None, parameters=None):
                 if "deviation_from" in policy_in_place.keys():
                     if policy_in_place["deviation_from"] == "previous":
                         new_date = np.max(past_policies) - datetime.timedelta(days=1)
-                        tax_data[param] = load_data(
-                            new_date,
-                            group,
-                            raw_group_data=raw_group_data,
-                            parameters=[param],
+                        tax_data[param] = _load_parameter_group_from_yaml(
+                            new_date, group, parameters=[param],
                         )[param]
                     elif "." in policy_in_place["deviation_from"]:
                         path_list = policy_in_place["deviation_from"].split(".")
-                        tax_data[param] = load_data(
-                            policy_date,
-                            path_list[0],
-                            raw_group_data=raw_group_data,
-                            parameters=[path_list[1]],
+                        tax_data[param] = _load_parameter_group_from_yaml(
+                            policy_date, path_list[0], parameters=[path_list[1]],
                         )[path_list[1]]
                     for key in value_keys:
                         key_list = []
@@ -258,7 +382,6 @@ def load_data(policy_date, group, raw_group_data=None, parameters=None):
                     for key in value_keys:
                         tax_data[param][key] = policy_in_place[key]
 
-    tax_data["jahr"] = policy_date.year
     tax_data["datum"] = policy_date
 
     return tax_data
@@ -266,7 +389,7 @@ def load_data(policy_date, group, raw_group_data=None, parameters=None):
 
 def transfer_dictionary(remaining_dict, new_dict, key_list):
     # To call recursive, always check if object is a dict
-    if type(remaining_dict) == dict:
+    if isinstance(remaining_dict, dict):
         for key in remaining_dict.keys():
             key_list_updated = key_list + [key]
             new_dict = transfer_dictionary(
