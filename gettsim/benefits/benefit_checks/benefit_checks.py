f80060fb11cc6ecc461c2637202d69b970fb286c
MaxBlesch <s6mables@uni-bonn.de>
2020-06-18 17:06:04 +0200

    Deleted some check functions.

--- gettsim/benefits/benefit_checks/benefit_checks.py
+++ gettsim/benefits/benefit_checks/benefit_checks.py
@@ -2,8 +2,20 @@ def wohngeld_m_vorrang(arbeitsl_geld_2_m_wohngeld_m, arbeitsl_geld_2_m_basis):
     return (arbeitsl_geld_2_m_wohngeld_m == 0) & (arbeitsl_geld_2_m_basis > 0)
 
 
-def kinderzuschlag_vorrang(arbeitsl_geld_2_m_kinderzuschlag, arbeitsl_geld_2_m_basis):
-    return (arbeitsl_geld_2_m_kinderzuschlag == 0) & (arbeitsl_geld_2_m_basis > 0)
+def kinderzuschlag_vorrang(
+    _regelbedarf_m_vermögens_check,
+    _sum_arbeitsl_geld_2_unterhaltsvors_kindergeld_m,
+    _kinderzuschlag_m_vermögens_check,
+    arbeitsl_geld_2_m_basis,
+):
+    rest_arbeitsl_geld_2 = (
+        _regelbedarf_m_vermögens_check
+        - (
+            _sum_arbeitsl_geld_2_unterhaltsvors_kindergeld_m
+            + _kinderzuschlag_m_vermögens_check
+        )
+    ).clip(lower=0)
+    return (rest_arbeitsl_geld_2 == 0) & (arbeitsl_geld_2_m_basis > 0)
 
 
 def wohngeld_m_kinderzuschlag_vorrang(
--- gettsim/benefits/benefit_checks/eink_checks.py
+++ gettsim/benefits/benefit_checks/eink_checks.py
@@ -2,6 +2,7 @@ def _sum_arbeitsl_geld_2_unterhaltsvors_kindergeld_m(
     arbeitsl_geld_2_eink_hh, unterhaltsvors_m_hh, kindergeld_m_hh
 ):
     sum_eink = arbeitsl_geld_2_eink_hh + unterhaltsvors_m_hh + kindergeld_m_hh
+    breakpoint()
     return sum_eink
 
 
@@ -11,15 +12,6 @@ def sum_wohngeld_m_arbeitsl_geld_2_eink(
     return (
         _sum_arbeitsl_geld_2_unterhaltsvors_kindergeld_m
         + _wohngeld_basis_hh_vermögens_check
-    )
-
-
-def sum_kinderzuschlag_arbeitsl_geld_2_eink(
-    _sum_arbeitsl_geld_2_unterhaltsvors_kindergeld_m, _kinderzuschlag_m_vermögens_check
-):
-    return (
-        _sum_arbeitsl_geld_2_unterhaltsvors_kindergeld_m
-        + _kinderzuschlag_m_vermögens_check
     )
 
 
@@ -41,14 +33,6 @@ def arbeitsl_geld_2_m_wohngeld_m_kinderzuschlag(
     return (
         _regelbedarf_m_vermögens_check
         - sum_wohngeld_m_kinderzuschlag_arbeitsl_geld_2_eink
-    ).clip(lower=0)
-
-
-def arbeitsl_geld_2_m_kinderzuschlag(
-    _regelbedarf_m_vermögens_check, sum_kinderzuschlag_arbeitsl_geld_2_eink
-):
-    return (
-        _regelbedarf_m_vermögens_check - sum_kinderzuschlag_arbeitsl_geld_2_eink
     ).clip(lower=0)
 
 
