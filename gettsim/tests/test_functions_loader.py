dbc53064c5a7e4f5ce6c3ec2d34c7069756ce63a
Tobias Raabe <raabe@posteo.de>
2020-07-30 12:37:05 +0200

    Add reduced form of function loader tests.

--- gettsim/tests/test_functions_loader.py
+++ gettsim/tests/test_functions_loader.py
@@ -1,7 +1,5 @@
-import textwrap
-
 from gettsim.config import ROOT_DIR
-from gettsim.functions_loader import _load_functions
+from gettsim.functions_loader import load_functions
 
 
 def func():
@@ -9,39 +7,14 @@ def func():
 
 
 def test_load_function():
-    assert _load_functions(func) == {"func": func}
+    assert load_functions(func) == {"func": func}
 
 
 def test_renaming_functions():
-    out = _load_functions([func, {"func_": func}])
+    out = load_functions([func, {"func_": func}])
     assert len(out) == 2
     assert "func" in out and "func_" in out
 
 
-def test_load_modules():
-    assert _load_functions("gettsim.soz_vers.krankenv_pflegev")
-
-
 def test_load_path():
-    assert _load_functions(ROOT_DIR / "soz_vers" / "krankenv_pflegev.py")
-
-
-def test_special_attribute_module_is_set(tmp_path):
-    py_file = """
-    def func():
-        pass
-    """
-    tmp_path.joinpath("functions.py").write_text(textwrap.dedent(py_file))
-
-    out = _load_functions(tmp_path.joinpath("functions.py"))
-    assert isinstance(out, dict)
-    assert "func" in out
-    assert len(out) == 1
-    assert out["func"].__module__ == "functions.py"
-
-
-def test_special_attribute_module_is_set_for_internal_functions():
-    out = _load_functions("gettsim.soz_vers.eink_grenzen")
-    function = out[list(out)[0]]
-
-    assert function.__module__ == "gettsim.soz_vers.eink_grenzen"
+    assert load_functions(ROOT_DIR / "soz_vers" / "krankenv_pflegev.py")
