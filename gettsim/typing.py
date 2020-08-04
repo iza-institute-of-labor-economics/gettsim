a957b84302193a30e8225e92f65f36211015a233
MaxBlesch <s6mables@uni-bonn.de>
2020-08-04 11:36:38 +0200

    Added typing module.

---
+++ gettsim/typing.py
@@ -0,0 +1,10 @@
+from typing import NewType
+
+import pandas as pd
+
+Series = NewType("Series", pd.Series)
+
+FloatSeries = Series[float]
+IntSeries = Series[int]
+BoolSeries = Series[bool]
+CatSeries = Series[int]
