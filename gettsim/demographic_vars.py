4545be2f222b4d61e7d5acbe7dae5506a78bbdfb
MaxBlesch <s6mables@uni-bonn.de>
2020-08-07 13:24:51 +0200

    Added ~…

--- gettsim/demographic_vars.py
+++ gettsim/demographic_vars.py
@@ -82,7 +82,7 @@ def gemeinsam_veranlagt(tu_id: IntSeries, _anz_erwachsene_tu: IntSeries) -> Bool
     tu_id
         See :ref:`tu_id`.
     _anz_erwachsene_tu
-        See :func:`demograhphic_vars._anz_erwachsene_tu`.
+        See :func:`~gettsim.demograhphic_vars._anz_erwachsene_tu`.
 
     Returns
     -------
