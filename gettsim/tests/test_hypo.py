35b99b94239ed46c76778b7a62c62ef96ea35c2a
Eric Sommer <ericsomm@gmx.de>
2020-10-18 18:25:46 +0200

    implemented in advanced_interface

--- gettsim/tests/test_hypo.py
+++ gettsim/tests/test_hypo.py
@@ -7,13 +7,9 @@ def test_hypo():
     """
     df = gettsim_hypo_data()
 
-    # only 1 or 2 adults per tax unit
-    assert df["anz_erwachsene_tu"].between(1, 2).all()
     # rent must be positive
     assert df["kaltmiete_m"].min() > 0
     # heating cost must be positive
     assert df["heizkost_m"].min() > 0
-    # Only 1 adult in single parent household
-    assert (df[df["hh_typ"].str.contains("sp")]["anz_erwachsene_hh"] == 1).all()
     # no NaN values
     assert df.notna().all().all()
