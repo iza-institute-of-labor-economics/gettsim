7091c54d00f08f58b1d0fab7e713bd5e7425fdcb
MaxBlesch <s6mables@uni-bonn.de>
2020-10-18 20:37:38 +0200

    Added docstring on check function.

--- gettsim/typing.py
+++ gettsim/typing.py
@@ -6,6 +6,20 @@ BoolSeries = TypeVar("BoolSeries")
 
 
 def check_if_series_has_internal_type(series, internal_type):
+    """Check if data type of series fits to the internal type of gettsim.
+
+    Parameters
+    ----------
+    series : pd.Series
+        Some data series.
+    internal_type : TypeVar
+        One of the internal gettsim types.
+
+    Returns
+    -------
+    out : bool
+        Return check variable.
+    """
     dtype = series.dtype
     if internal_type == FloatSeries:
         # I think we can allow for ints here?!
