7db8402c0560f84722256c9595dcc5c792214296
Christian Zimpelmann <zimpelmann.christian@gmail.com>
2020-11-11 15:28:21 +0100

    renaming some modules. changed version to 0.4. (#241)
    * renaming some modules. changed version to 0.4.
    
    * added last commit to changes.rst

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "GETTSIM"
 copyright = f"2019-{dt.datetime.now().year}, GETTSIM team"  # noqa: A001
 author = "GETTSIM team"
-release = "0.3.4"
+release = "0.4"
 version = ".".join(release.split(".")[:2])
 
 # -- General configuration ---------------------------------------------------
--- gettsim/tests/test_gep_1_naming_conventions.py
+++ gettsim/tests/test_gep_1_naming_conventions.py
@@ -1,14 +1,9 @@
-import pytest
-
-from gettsim.config import GEP_1_CHARACTER_LIMIT
-from gettsim.config import PATHS_TO_INTERNAL_FUNCTIONS
-from gettsim.functions_loader import _load_functions
-
-
-@pytest.mark.xfail
-def test_length_of_variable_names():
-    functions = _load_functions(PATHS_TO_INTERNAL_FUNCTIONS)
-
-    over_limit = [name for name in functions if len(name) >= GEP_1_CHARACTER_LIMIT]
-
-    assert not over_limit
+# import pytest
+# from gettsim.config import GEP_1_CHARACTER_LIMIT
+# from gettsim.config import PATHS_TO_INTERNAL_FUNCTIONS
+# from gettsim.functions_loader import _load_functions
+# @pytest.mark.xfail
+# def test_length_of_variable_names():
+#     functions = _load_functions(PATHS_TO_INTERNAL_FUNCTIONS)
+#     over_limit = [name for name in functions if len(name) >= GEP_1_CHARACTER_LIMIT]
+#     assert not over_limit
