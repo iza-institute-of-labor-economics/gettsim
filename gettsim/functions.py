80457db7d21536dfeba8a68ae26e35033323646b
Christian Zimpelmann <zimpelmann.christian@gmail.com>
2021-10-01 17:03:29 +0200

    added grundsicherung to functions.py

--- gettsim/functions.py
+++ gettsim/functions.py
@@ -27,6 +27,7 @@ from gettsim.transfers.arbeitsl_geld_2.kost_unterk import *  # noqa: F401, F403
 from gettsim.transfers.benefit_checks.benefit_checks import *  # noqa: F401, F403
 from gettsim.transfers.benefit_checks.vermoegens_checks import *  # noqa: F401, F403
 from gettsim.transfers.elterngeld import *  # noqa: F401, F403
+from gettsim.transfers.grundsicherung import *  # noqa: F401, F403
 from gettsim.transfers.kinderzuschlag.kinderzuschlag import *  # noqa: F401, F403
 from gettsim.transfers.kinderzuschlag.kinderzuschlag_eink import *  # noqa: F401, F403
 from gettsim.transfers.kinderzuschlag.kost_unterk import *  # noqa: F401, F403
