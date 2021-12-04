0ec16d8af6af79a8cbeeb8e262399325e221ca22
Eric Sommer <ericsomm@gmx.de>
2021-12-04 22:22:02 +0100

    eink_st works again

--- gettsim/taxes/eink_st.py
+++ gettsim/taxes/eink_st.py
@@ -1,16 +1,21 @@
 from gettsim.piecewise_functions import piecewise_polynomial
+from gettsim.typing import FloatSeries
+from gettsim.typing import IntSeries
 
 
 def st_kein_kind_freib_tu(
-    zu_verst_eink_kein_kinderfreib_tu, anz_erwachsene_tu, eink_st_params
-):
+    zu_verst_eink_kein_kinderfreib_tu: FloatSeries,
+    anz_erwachsene_tu: IntSeries,
+    eink_st_params: dict,
+) -> FloatSeries:
     """ Taxes without child allowance.
 
     Parameters
     ----------
-    _zu_verst_eink_kein_kinderfreib_tu
-    _anz_erwachsene_tu
-    eink_st_params
+   zu_verst_eink_kein_kinderfreib_tu
+        See :func:`zu_verst_eink_kein_kinderfreib_tu`.
+    anz_erwachsene_tu
+        See :func:`anz_erwachsene_tu`.
 
     Returns
     -------
@@ -21,14 +26,21 @@ def st_kein_kind_freib_tu(
     return anz_erwachsene_tu * st_tarif(zu_verst_eink_per_indiv, params=eink_st_params)
 
 
-def st_kind_freib_tu(zu_verst_eink_kinderfreib_tu, anz_erwachsene_tu, eink_st_params):
+def st_kind_freib_tu(
+    zu_verst_eink_kinderfreib_tu: FloatSeries,
+    anz_erwachsene_tu: IntSeries,
+    eink_st_params: dict,
+) -> FloatSeries:
     """Taxes with child allowance.
 
     Parameters
     ----------
-    _zu_verst_eink_kinderfreib_tu
-    _anz_erwachsene_tu
+    zu_verst_eink_kinderfreib_tu
+        See :func:`zu_verst_eink_kinderfreib_tu`.
+    anz_erwachsene_tu
+        See :func:`anz_erwachsene_tu`.
     eink_st_params
+        See params documentation :ref:`eink_st_params <eink_st_params>`.
 
     Returns
     -------
@@ -39,17 +51,18 @@ def st_kind_freib_tu(zu_verst_eink_kinderfreib_tu, anz_erwachsene_tu, eink_st_pa
     return anz_erwachsene_tu * st_tarif(zu_verst_eink_per_indiv, params=eink_st_params)
 
 
-def st_tarif(x, params):
+def st_tarif(x: FloatSeries, params: dict) -> FloatSeries:
     """ The German Income Tax Tariff.
      Modelled only after 2002 so far.
 
-    It's not calculated as in the tax code, but rather a gemoetric decomposition of the
-    area beneath the marginal tax rate function.
-    This facilitates the implementation of alternative tax schedules
-
-    args:
-        x (Series): taxable income
-        params (dict): tax-benefit parameters specific to year and reform
+    Parameters
+    ----------
+    x : Floatseries
+        Some floatseries wherest_tarif is applied to.
+    params : dict
+        Dictionary created in respy.piecewise_functions.
+    Returns
+    -------
     """
     eink_st = piecewise_polynomial(
         x=x,
