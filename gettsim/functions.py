460c0a609b11512ad578691f127a2beaef6250bb
Christian Zimpelmann <zimpelmann.christian@gmail.com>
2022-04-21 16:34:48 +0200

    remove Umlaute from file names. (#383)

--- gettsim/functions.py
+++ gettsim/functions.py
@@ -13,7 +13,7 @@ from gettsim.taxes.abgelt_st import *  # noqa: F401, F403
 from gettsim.taxes.eink_st import *  # noqa: F401, F403
 from gettsim.taxes.soli_st import *  # noqa: F401, F403
 from gettsim.taxes.zu_verst_eink.eink import *  # noqa: F401, F403
-from gettsim.taxes.zu_verst_eink.freibeträge import *  # noqa: F401, F403
+from gettsim.taxes.zu_verst_eink.freibetraege import *  # noqa: F401, F403
 from gettsim.taxes.zu_verst_eink.vorsorgeaufw import *  # noqa: F401, F403
 from gettsim.taxes.zu_verst_eink.zu_verst_eink import *  # noqa: F401, F403
 from gettsim.transfers.arbeitsl_geld import *  # noqa: F401, F403
@@ -21,7 +21,7 @@ from gettsim.transfers.arbeitsl_geld_2.arbeitsl_geld_2 import *  # noqa: F401, F
 from gettsim.transfers.arbeitsl_geld_2.arbeitsl_geld_2_eink import *  # noqa: F401, F403
 from gettsim.transfers.arbeitsl_geld_2.kost_unterk import *  # noqa: F401, F403
 from gettsim.transfers.benefit_checks.benefit_checks import *  # noqa: F401, F403
-from gettsim.transfers.benefit_checks.vermögens_checks import *  # noqa: F401, F403
+from gettsim.transfers.benefit_checks.vermoegens_checks import *  # noqa: F401, F403
 from gettsim.transfers.elterngeld import *  # noqa: F401, F403
 from gettsim.transfers.grundrente import *  # noqa: F401, F403
 from gettsim.transfers.grunds_im_alter import *  # noqa: F401, F403
