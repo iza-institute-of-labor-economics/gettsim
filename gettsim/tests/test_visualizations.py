088c19b9cc675450af3d28a302603816a245a78f
Christian Zimpelmann <zimpelmann.christian@gmail.com>
2022-08-25 10:28:25 +0200

    comment tests visualization back in.

--- gettsim/tests/test_visualizations.py
+++ gettsim/tests/test_visualizations.py
@@ -6,9 +6,8 @@ from gettsim.visualization import _get_selected_nodes
 from gettsim.visualization import _kth_order_neighbors
 from gettsim.visualization import _node_and_ancestors
 from gettsim.visualization import _node_and_descendants
-
-# from gettsim.visualization import _select_nodes_in_dag
-# from gettsim.visualization import plot_dag
+from gettsim.visualization import _select_nodes_in_dag
+from gettsim.visualization import plot_dag
 
 policy_params, policy_functions = set_up_policy_environment(date=2020)
 
@@ -81,66 +80,66 @@ def test_get_selected_nodes(n_nodes, selector, expected):
     assert nodes == expected
 
 
-# @pytest.mark.parametrize(
-#     "n_nodes, selectors, expected",
-#     [
-#         (5, [{"node": [1, 2], "type": "nodes"}], {1, 2}),
-#         (
-#             5,
-#             [{"node": [0], "type": "nodes"}, {"node": 3, "type": "descendants"}],
-#             {0, 3, 4, 5},
-#         ),
-#         (
-#             5,
-#             [
-#                 {"node": [0], "type": "nodes"},
-#                 {"node": 3, "type": "descendants"},
-#                 {"node": [4], "type": "nodes", "select": False},
-#             ],
-#             {0, 3, 5},
-#         ),
-#     ],
-# )
-# def test_select_nodes_in_dag(n_nodes, selectors, expected):
-#     dag = nx.DiGraph([(i, i + 1) for i in range(n_nodes)])
-#     dag = _select_nodes_in_dag(dag, selectors)
-#     assert set(dag.nodes) == expected
+@pytest.mark.parametrize(
+    "n_nodes, selectors, expected",
+    [
+        (5, [{"node": [1, 2], "type": "nodes"}], {1, 2}),
+        (
+            5,
+            [{"node": [0], "type": "nodes"}, {"node": 3, "type": "descendants"}],
+            {0, 3, 4, 5},
+        ),
+        (
+            5,
+            [
+                {"node": [0], "type": "nodes"},
+                {"node": 3, "type": "descendants"},
+                {"node": [4], "type": "nodes", "select": False},
+            ],
+            {0, 3, 5},
+        ),
+    ],
+)
+def test_select_nodes_in_dag(n_nodes, selectors, expected):
+    dag = nx.DiGraph([(i, i + 1) for i in range(n_nodes)])
+    dag = _select_nodes_in_dag(dag, selectors)
+    assert set(dag.nodes) == expected
 
 
-# def test_plot_dag():
-#     """Make sure that minimal example doesn't produce an error"""
-#     plot_dag(functions=[], targets=["erwachsene_alle_rentner_hh"])
+def test_plot_dag():
+    """Make sure that minimal example doesn't produce an error"""
+    plot_dag(functions=[], targets=["erwachsene_alle_rentner_hh"])
 
 
-# def test_one_dot_plot_dag():
-#     """Make sure that the one dot graph example doesn't produce an error"""
-#     selectors = "kapitaleink_brutto_tu"
-#     plot_dag(functions=policy_functions, selectors=selectors)
+def test_one_dot_plot_dag():
+    """Make sure that the one dot graph example doesn't produce an error"""
+    selectors = "kapitaleink_brutto_tu"
+    plot_dag(functions=policy_functions, selectors=selectors)
 
 
-# def test_10_dots_plot_dag():
-#     """Make sure that when No.of nodes is larger than 10 or show_labels is false,
-#     the graph example doesn't produce an error and hover information works properly"""
-#     selector = {"type": "descendants", "node": "geringfügig_beschäftigt"}
-#     plot_dag(functions=policy_functions, selectors=selector, orientation="h")
+def test_10_dots_plot_dag():
+    """Make sure that when No.of nodes is larger than 10 or show_labels is false,
+    the graph example doesn't produce an error and hover information works properly"""
+    selector = {"type": "descendants", "node": "geringfügig_beschäftigt"}
+    plot_dag(functions=policy_functions, selectors=selector, orientation="h")
 
 
-# def test_horizontal_plot_dag():
-#     """Make sure that when we choose horizontal orientation,
-#     the graph example doesn't produce an error"""
-#     plot_dag(
-#         functions=policy_functions,
-#         selectors=[{"node": "zu_verst_kapitaleink_tu", "type": "neighbors"}],
-#         orientation="h",
-#     )
+def test_horizontal_plot_dag():
+    """Make sure that when we choose horizontal orientation,
+    the graph example doesn't produce an error"""
+    plot_dag(
+        functions=policy_functions,
+        selectors=[{"node": "zu_verst_kapitaleink_tu", "type": "neighbors"}],
+        orientation="h",
+    )
 
 
-# def test_hover_source_code_plot_dag():
-#     """Make sure that when hover information is source code,
-#     the graph example doesn't produce an error and works properly"""
-#     plot_dag(
-#         functions=policy_functions,
-#         selectors=[{"node": "zu_verst_kapitaleink_tu", "type": "neighbors"}],
-#         orientation="h",
-#         hover_source_code=True,
-#     )
+def test_hover_source_code_plot_dag():
+    """Make sure that when hover information is source code,
+    the graph example doesn't produce an error and works properly"""
+    plot_dag(
+        functions=policy_functions,
+        selectors=[{"node": "zu_verst_kapitaleink_tu", "type": "neighbors"}],
+        orientation="h",
+        hover_source_code=True,
+    )
