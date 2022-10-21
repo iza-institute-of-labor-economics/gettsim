002e7284e9cacd75e9b0ba3ee5aabe153fa54f6e
Tim <tim@mensinger96.com>
2022-10-21 15:13:26 +0200

    Replace mapping with additional transformer method

--- gettsim/jax.py
+++ gettsim/jax.py
@@ -3,40 +3,36 @@ import functools
 import inspect
 from copy import deepcopy
 from importlib import import_module
-from typing import Dict
 
 import astor
 
 
-DEFAULT_MAPPING = {" and ": " & ", " or ": " | ", " not ": " ~"}
+BACKEND_TO_MODULE = {"jax": "jax.numpy", "numpy": "numpy"}
 
 
-def change_if_to_where_wrapper(
-    func: callable, backend: str = "np", mapping: Dict[str, str] = DEFAULT_MAPPING
-):
-    """Change if statement to where call in the ast of func and return new function.
+def make_vectorizable(func: callable, backend: str):
+    """Redefine function to be vectorizable given backend.
+
+    This function replaces if statements with {backend}.where(), as well as boolean
+    operations with their bit operation counterpart, and not with inversion. After the
+    replacement the function can be called with vector-valued input.
 
     Args:
         func (callable): Function.
-        backend (str): Backend array library abbreviation.
-        mapping (dict): Find-and-replace mapping which is applied to the source code
-            before the ast is generated.
+        backend (str): Backend library. See dict `BACKEND_TO_MODULE` for currently
+            supported backends. Array module must export function `where` that behaves
+            as `numpy.where`.
 
     Returns:
         callable: New function with altered ast.
 
     """
-    tree = _change_if_to_where_ast(func, backend=backend, mapping=mapping)
+    module = _module_from_backend(backend)
+    tree = _make_vectorizable_ast(func, module=module)
 
     # recreate scope of function and add array library
     scope = func.__globals__
-    if backend == "np":
-        scope["np"] = import_module("numpy")
-    elif backend == "jnp":
-        scope["jnp"] = import_module("jax.numpy")
-    else:
-        msg = f"Argument 'backend' is {backend} but must be in {'np', 'jnp'}."
-        raise NotImplementedError(msg)
+    scope[module] = import_module(module)
 
     # execute new ast
     compiled = compile(tree, "<ast>", "exec")
@@ -47,60 +43,56 @@ def change_if_to_where_wrapper(
     return functools.wraps(func)(new_func)
 
 
-def change_if_to_where_source(
-    func: callable, backend: str = "np", mapping: Dict[str, str] = DEFAULT_MAPPING
+def make_vectorizable_source(
+    func: callable,
+    backend: str,
 ):
-    """Change if statement to where call in the ast of func and return new source code.
+    """Redefine function source to be vectorizable given backend.
 
-    Note: This function only works when the package astor is installed.
+    This function replaces if statements with {backend}.where(), as well as boolean
+    operations with their bit operation counterpart, and not with inversion. After the
+    replacement the function can be called with vector-valued input.
 
     Args:
         func (callable): Function.
-        backend (str): Backend array library abbreviation.
-        mapping (dict): Find-and-replace mapping which is applied to the source code
-            before the ast is generated.
+        backend (str): Backend library. See dict `BACKEND_TO_MODULE` for currently
+            supported backends. Array module must export function `where` that behaves
+            as `numpy.where`.
 
     Returns:
         str: Source code of new function with altered ast.
 
     """
-    tree = _change_if_to_where_ast(func, backend=backend, mapping=mapping)
+    module = _module_from_backend(backend)
+    tree = _make_vectorizable_ast(func, module=module)
     source = astor.code_gen.to_source(tree)
     return source
 
 
-def _change_if_to_where_ast(
-    func: callable, backend: str = "np", mapping: Dict[str, str] = DEFAULT_MAPPING
-):
+def _make_vectorizable_ast(func: callable, module: str):
     """Change if statement to where call in the ast of func and return new ast.
 
     Args:
         func (callable): Function.
-        backend (str): Backend array library abbreviation.
-        mapping (dict): Find-and-replace mapping which is applied to the source code
-            before the ast is generated.
+        module (str): Module which exports the function `where` that behaves as
+            `numpy.where`.
 
     Returns:
         ast.Module: AST of new function with altered ast.
 
     """
     # create ast from func
-    tree = _func_to_ast(func, mapping=mapping)
+    tree = _func_to_ast(func)
     tree = _add_parent_attr_to_ast(tree)
 
     # transform tree nodes
-    new_tree = Transformer(backend).visit(tree)
+    new_tree = Transformer(module).visit(tree)
     new_tree = ast.fix_missing_locations(new_tree)
     return new_tree
 
 
-def _func_to_ast(func: callable, mapping: Dict = None):
+def _func_to_ast(func: callable):
     source = inspect.getsource(func)
-
-    if mapping is not None:
-        for old, new in mapping.items():
-            source = source.replace(old, new)
-
     tree = ast.parse(source)
     return tree
 
@@ -114,48 +106,48 @@ def _add_parent_attr_to_ast(tree: ast.AST):
 
 
 # ======================================================================================
-# Transformation classes and functions
+# Transformation class and corresponding functions
 # ======================================================================================
 
 
 class Transformer(ast.NodeTransformer):
-    """Transformer class.
+    def __init__(self, module: str):
+        self.module = module
 
-    The visit_{stmt|expr} methods are called while traversing the ast if the current
-    node class is {stmt|expr}. The node is then swapped with the return value of
-    visit_{stmt|expr}.
+    def visit_Not(self, node: ast.Not):  # noqa: N802, U100
+        return ast.Invert()
 
-    """
+    def visit_BoolOp(self, node: ast.BoolOp):  # noqa: N802
+        self.generic_visit(node)
+        binop = _boolop_to_binop(node)
+        return binop
 
-    def __init__(self, backend: str):
-        self.backend = backend
-
-    def visit_If(self, node: ast.AST):  # noqa: N802
-        if isinstance(node.parent, ast.FunctionDef):
-            call = _if_to_call(node, backend=self.backend)
-            if isinstance(node.body[0], ast.Return):
-                out = ast.Return(call)
-            elif isinstance(node.body[0], (ast.Assign, ast.AugAssign)):
-                out = deepcopy(node.body[0])
-                out.value = call
-        else:
-            out = node
+    def visit_If(self, node: ast.If):  # noqa: N802
+        self.generic_visit(node)
+        call = _if_to_call(node, module=self.module)
+        if isinstance(node.body[0], ast.Return):
+            out = ast.Return(call)
+        elif isinstance(node.body[0], (ast.Assign, ast.AugAssign)):
+            out = deepcopy(node.body[0])
+            out.value = call
         return out
 
-    def visit_IfExp(self, node: ast.AST):  # noqa: N802
-        call = _ifexp_to_call(node, backend=self.backend)
+    def visit_IfExp(self, node: ast.IfExp):  # noqa: N802
+        self.generic_visit(node)
+        call = _ifexp_to_call(node, module=self.module)
         return call
 
 
-def _if_to_call(node: ast.If, backend: str):
+def _if_to_call(node: ast.If, module: str):
     """Transform If statement to Call.
 
     Args:
-        node (ast.If): A If node in the ast.
-        backend (str): Backend array library.
+        node (ast.If): An If node in the ast.
+        module (str): Module which exports the function `where` that behaves as
+            `numpy.where`.
 
     Returns:
-        ast.Call: The If statement reformatted using a call to {backend}.where().
+        ast.Call: The If statement reformatted using a call to {module}.where().
 
     """
     args = [node.test, node.body[0].value]
@@ -175,11 +167,11 @@ def _if_to_call(node: ast.If, backend: str):
     elif isinstance(node.orelse[0], ast.Return):
         args.append(node.orelse[0].value)
     elif isinstance(node.orelse[0], ast.If):
-        call = _if_to_call(node.orelse[0], backend=backend)
+        call = _if_to_call(node.orelse[0], module=module)
         args.append(call)
     elif isinstance(node.orelse[0], (ast.Assign, ast.AugAssign)):
         if isinstance(node.orelse[0].value, ast.IfExp):
-            call = _ifexp_to_call(node.orelse[0].value, backend=backend)
+            call = _ifexp_to_call(node.orelse[0].value, module=module)
             args.append(call)
         else:
             args.append(node.orelse[0].value)
@@ -189,7 +181,7 @@ def _if_to_call(node: ast.If, backend: str):
 
     call = ast.Call(
         func=ast.Attribute(
-            value=ast.Name(id=backend, ctx=ast.Load()), attr="where", ctx=ast.Load()
+            value=ast.Name(id=module, ctx=ast.Load()), attr="where", ctx=ast.Load()
         ),
         args=args,
         keywords=[],
@@ -197,33 +189,65 @@ def _if_to_call(node: ast.If, backend: str):
     return call
 
 
-def _ifexp_to_call(node: ast.IfExp, backend: str):
+def _ifexp_to_call(node: ast.IfExp, module: str):
     """Transform IfExp expression to Call.
 
     Args:
-        node (ast.IfExp): A IfExp node in the ast.
-        backend (str): Backend array library.
+        node (ast.IfExp): An IfExp node in the ast.
+        module (str): Module which exports the function `where` that behaves as
+            `numpy.where`.
 
     Returns:
-        ast.Call: The IfExp expression reformatted using a call to {backend}.where().
+        ast.Call: The IfExp expression reformatted using a call to {module}.where().
 
     """
     args = [node.test, node.body]
 
     if isinstance(node.orelse, ast.IfExp):
-        call = _ifexp_to_call(node.orelse, backend=backend)
+        call = _ifexp_to_call(node.orelse, module=module)
         args.append(call)
     else:
         args.append(node.orelse)
 
     call = ast.Call(
         func=ast.Attribute(
-            value=ast.Name(id=backend, ctx=ast.Load()), attr="where", ctx=ast.Load()
+            value=ast.Name(id=module, ctx=ast.Load()), attr="where", ctx=ast.Load()
         ),
         args=args,
         keywords=[],
     )
     return call
+
+
+def _boolop_to_binop(node: ast.BoolOp):
+    """Transform BoolOp operation to BinOp.
+
+    Args:
+        node (ast.BoolOp): A BoolOp node in the ast.
+
+    Returns:
+        ast.BinOp: The BoolOp expression reformatted using '&' instead of 'and', and '|'
+            instead of 'or'.
+
+    """
+    binop = ast.BinOp()
+
+    if isinstance(node.values[0], ast.BoolOp):
+        binop.left = _boolop_to_binop(node.values[0])
+    else:
+        binop.left = node.values[0]
+
+    if isinstance(node.values[1], ast.BoolOp):
+        binop.right = _boolop_to_binop(node.values[1])
+    else:
+        binop.right = node.values[1]
+
+    if isinstance(node.op, ast.And):
+        binop.op = ast.BitAnd()
+    elif isinstance(node.op, ast.Or):
+        binop.op = ast.BitOr()
+
+    return binop
 
 
 # ======================================================================================
@@ -275,3 +299,14 @@ def _node_to_formatted_source(node: ast.AST):
     source = astor.code_gen.to_source(node)
     source = " > " + source[:-1].replace("\n", "\n > ")
     return source
+
+
+def _module_from_backend(backend: str):
+    module = BACKEND_TO_MODULE.get(backend, None)
+    if module is None:
+        msg = (
+            f"Argument 'backend' is {backend} but must be in "
+            f"{BACKEND_TO_MODULE.keys()}."
+        )
+        raise NotImplementedError(msg)
+    return module
--- gettsim/tests/test_jax.py
+++ gettsim/tests/test_jax.py
@@ -1,11 +1,11 @@
 import inspect
 
-import numpy as np
+import numpy
 import pytest
 from numpy.testing import assert_array_equal
 
-from gettsim.jax import change_if_to_where_source
-from gettsim.jax import change_if_to_where_wrapper
+from gettsim.jax import make_vectorizable
+from gettsim.jax import make_vectorizable_source
 from gettsim.jax import TranslateToJaxError
 
 
@@ -22,7 +22,7 @@ def f1(x):
 
 
 def f1_exp(x):
-    return np.where(x < 0, 0, 1)
+    return numpy.where(x < 0, 0, 1)
 
 
 def f2(x):
@@ -34,7 +34,7 @@ def f2(x):
 
 
 def f2_exp(x):
-    out = np.where(x < 0, 0, 1)
+    out = numpy.where(x < 0, 0, 1)
     return out
 
 
@@ -43,7 +43,7 @@ def f3(x):
 
 
 def f3_exp(x):
-    return np.where(x < 0, 0, 1)
+    return numpy.where(x < 0, 0, 1)
 
 
 def f4(x):
@@ -55,7 +55,7 @@ def f4(x):
 
 def f4_exp(x):
     out = 1
-    out = np.where(x < 0, 0, out)
+    out = numpy.where(x < 0, 0, out)
     return out
 
 
@@ -70,7 +70,7 @@ def f5(x):
 
 
 def f5_exp(x):
-    out = np.where(x < 0, -1, np.where(x > 0, 1, 0))
+    out = numpy.where(x < 0, -1, numpy.where(x > 0, 1, 0))
     return out
 
 
@@ -83,16 +83,18 @@ def f6(flag, another_flag):
 
 
 def f6_exp(flag, another_flag):
-    out = np.where(flag & ~another_flag, 1, 0)
+    out = numpy.where(flag & ~another_flag, 1, 0)
     return out
 
 
 def f7(x):
-    return 0 if x < 0 else 1
+    out = 0 if x < 0 else 1
+    return out
 
 
 def f7_exp(x):
-    return np.where(x < 0, 0, 1)
+    out = numpy.where(x < 0, 0, 1)
+    return out
 
 
 def f8(x):
@@ -100,13 +102,13 @@ def f8(x):
 
 
 def f8_exp(x):
-    return np.where(x < 0, -1, np.where(x > 0, 1, 0))
+    return numpy.where(x < 0, -1, numpy.where(x > 0, 1, 0))
 
 
 # expect no change since there is no if-clause and no [and|or] statement.
 def f9(x):
-    y = np.sum(x)
-    z = np.prod(x)
+    y = numpy.sum(x)
+    z = numpy.prod(x)
     return y * z
 
 
@@ -131,7 +133,7 @@ def f11(x):
 
 
 def f11_exp(x):
-    out = np.where(x < 0, -1, np.where(x > 0, 1, 0))
+    out = numpy.where(x < 0, -1, numpy.where(x > 0, 1, 0))
     return out
 
 
@@ -144,12 +146,12 @@ def f12(x):
 
 def f12_exp(x):
     out = 0
-    out += np.where(x < 1, 1, out)
+    out += numpy.where(x < 1, 1, out)
     return out
 
 
-x = np.arange(-10, 10)
-rng = np.random.default_rng(seed=0)
+x = numpy.arange(-10, 10)
+rng = numpy.random.default_rng(seed=0)
 flag = rng.binomial(1, 0.25, size=100)
 another_flag = rng.binomial(1, 0.75, size=100)
 
@@ -180,13 +182,13 @@ TEST_CASES = [
 def test_change_if_to_where_source(func, expected, args):  # noqa: U100
     exp = inspect.getsource(expected)
     exp = exp.replace("_exp", "")
-    got = change_if_to_where_source(func, backend="np")
+    got = make_vectorizable_source(func, backend="numpy")
     assert exp == got
 
 
 @pytest.mark.parametrize("func, expected, args", TEST_CASES)
 def test_change_if_to_where_wrapper(func, expected, args):
-    got_func = change_if_to_where_wrapper(func, backend="np")
+    got_func = make_vectorizable(func, backend="numpy")
     got = got_func(*args)
     exp = expected(*args)
     assert_array_equal(got, exp)
@@ -209,17 +211,17 @@ def g1(x):
 
 def test_too_many_operations_error_source():
     with pytest.raises(TranslateToJaxError):
-        change_if_to_where_source(g1)
+        make_vectorizable_source(g1, backend="numpy")
 
 
 def test_too_many_operations_error_wrapper():
     with pytest.raises(TranslateToJaxError):
-        change_if_to_where_wrapper(g1)
+        make_vectorizable(g1, backend="numpy")
 
 
 def test_notimplemented_error():
     with pytest.raises(NotImplementedError):
-        change_if_to_where_wrapper(f1, backend="dask")
+        make_vectorizable(f1, backend="dask")
 
 
 def g2(x):
@@ -240,22 +242,10 @@ def g3(x):
 @pytest.mark.parametrize("func", [g2, g3])
 def test_unallowed_operation_source(func):
     with pytest.raises(TranslateToJaxError):
-        change_if_to_where_source(func)
+        make_vectorizable_source(func, backend="numpy")
 
 
 @pytest.mark.parametrize("func", [g2, g3])
 def test_unallowed_operation_wrapper(func):
     with pytest.raises(TranslateToJaxError):
-        change_if_to_where_wrapper(func)
-
-
-def g4(x):
-    # no brackets around logical expression
-    flag = x < 0 and x > -1
-    return flag
-
-
-def test_missing_brackets():
-    with pytest.raises(ValueError):
-        g = change_if_to_where_wrapper(g4)
-        g(x)
+        make_vectorizable(func, backend="numpy")
