9aac070e45bb075e003db3acf8d89037c206f9ab
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2023-01-10 15:23:21 +0100

    Make config tests pass.

--- src/_gettsim_tests/test_config.py
+++ src/_gettsim_tests/test_config.py
@@ -8,20 +8,27 @@ def test_default_backend():
     assert numpy_or_jax.__name__ == "numpy"
 
 
-def test_set_backend(monkeypatch):
-    # act like jax is installed so that tests run on windows
-    monkeypatch.setattr(_gettsim.config, "IS_JAX_INSTALLED", True)
+def test_set_backend():
+    jax_is_truly_installed = _gettsim.config.IS_JAX_INSTALLED
 
     # expect default backend
     from _gettsim.config import numpy_or_jax
 
     assert numpy_or_jax.__name__ == "numpy"
 
-    # set jax backend
-    _gettsim.config.set_array_backend("jax")
-    from _gettsim.config import numpy_or_jax
+    if jax_is_truly_installed:
+        # set jax backend
+        _gettsim.config.set_array_backend("jax")
+        from _gettsim.config import numpy_or_jax
 
-    assert numpy_or_jax.__name__ == "jax.numpy"
+        assert numpy_or_jax.__name__ == "jax.numpy"
+
+        from _gettsim.config import USE_JAX
+
+        assert USE_JAX
+    else:
+        with pytest.raises(ValueError):
+            _gettsim.config.set_array_backend("jax")
 
 
 @pytest.mark.parametrize("backend", ["dask", "jax.numpy"])
