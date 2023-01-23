5be57f85f9c6130384f25a3e5addb538fa5c89bd
Christian Zimpelmann <zimpelmann.christian@gmail.com>
2023-01-23 10:57:41 +0100

    finish renaming of unterhalt.py to unterhaltsv.py.

--- src/_gettsim/functions.py
+++ src/_gettsim/functions.py
@@ -29,5 +29,5 @@ from _gettsim.transfers.kinderzuschl.kinderzuschl import *  # noqa: F401, F403
 from _gettsim.transfers.kinderzuschl.kinderzuschl_eink import *  # noqa: F401, F403
 from _gettsim.transfers.kinderzuschl.kost_unterk import *  # noqa: F401, F403
 from _gettsim.transfers.rente import *  # noqa: F401, F403
-from _gettsim.transfers.unterhalt import *  # noqa: F401, F403
+from _gettsim.transfers.unterhaltsv import *  # noqa: F401, F403
 from _gettsim.transfers.wohngeld import *  # noqa: F401, F403
