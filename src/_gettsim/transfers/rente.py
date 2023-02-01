6f0a2cebc913f7a4729cba50e6a4fbadfdb63812
Christian Zimpelmann <zimpelmann.christian@gmail.com>
2023-02-01 19:22:38 +0100

    A couple of renamings to be consistent with current naming convention. (#517)
    * rename vor to bis to be consistent with naming convention.
    
    * some more renamings.
    
    * adjust type hint.

--- src/_gettsim/transfers/rente.py
+++ src/_gettsim/transfers/rente.py
@@ -216,7 +216,7 @@ def ges_rente_zugangsfaktor(
     ges_rente_regelaltersgrenze: float,
     referenz_alter_abschlag: float,
     _ges_rente_altersgrenze_abschlagsfrei: float,
-    ges_rente_vorauss_vorzeitig: float,
+    ges_rente_vorauss_vorzeitig: bool,
     ges_rente_vorauss_regelrente: bool,
     ges_rente_params: dict,
 ) -> float:
@@ -557,7 +557,7 @@ def _ges_rente_besond_langj_altersgrenze(
 def ges_rente_vorauss_vorzeitig(
     ges_rente_vorauss_frauen: bool,
     ges_rente_vorauss_langj: bool,
-) -> float:
+) -> bool:
     """Function determining eligibility for early retirement. Can only be claimed if
     eligible for "Rente für langjährig Versicherte".
 
