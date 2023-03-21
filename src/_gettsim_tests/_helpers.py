92e9f46dbafe2906c78ad68ab9d7da23422adbb5
Lars Reimann <mail@larsreimann.com>
2023-03-21 16:57:35 +0100

    test: normalize `date` argument before caching

--- src/_gettsim_tests/_helpers.py
+++ src/_gettsim_tests/_helpers.py
@@ -2,9 +2,14 @@ from datetime import datetime
 from functools import lru_cache
 from typing import Union
 
-from _gettsim.policy_environment import set_up_policy_environment
+from _gettsim.policy_environment import set_up_policy_environment, _parse_date
+
+
+def cached_set_up_policy_environment(date: Union[int, str, datetime.date]) -> tuple[dict, dict]:
+    normalized_date = _parse_date(date)
+    return _cached_set_up_policy_environment(normalized_date)
 
 
 @lru_cache(maxsize=100)
-def cached_set_up_policy_environment(date: Union[int, str, datetime.date]) -> tuple[dict, dict]:
+def _cached_set_up_policy_environment(date: datetime.date) -> tuple[dict, dict]:
     return set_up_policy_environment(date)
