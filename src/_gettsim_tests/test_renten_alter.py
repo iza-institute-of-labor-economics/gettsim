a2602243be9404d26dd53200027f45dddc0a8ebd
Lars Reimann <mail@larsreimann.com>
2023-04-03 17:20:33 +0200

    test: check all columns of `renten_alter` at once

--- src/_gettsim_tests/test_renten_alter.py
+++ src/_gettsim_tests/test_renten_alter.py
@@ -1,7 +1,7 @@
 import itertools
 
 import pytest
-from pandas.testing import assert_series_equal
+from pandas._testing import assert_frame_equal
 
 from _gettsim.interface import compute_taxes_and_transfers
 from _gettsim_tests._helpers import cached_set_up_policy_environment
@@ -13,31 +13,26 @@ data = load_policy_test_data("renten_alter")
 
 
 @pytest.mark.parametrize(
-    "year",
-    YEARS,
+    ("year", "test_data"),
+    itertools.product(YEARS, data.test_data),
+    ids=str,
 )
-class TestRentenAlter:
+def test_renten_alter(
+        year: int,
+        test_data: PolicyTestData,
+):
+    output_columns = test_data.output_df.columns
 
-    @pytest.mark.parametrize(
-        ("test_data", "column"),
-        data.parametrize_args,
-        ids=str,
+    df = test_data.input_df
+
+    policy_params, policy_functions = cached_set_up_policy_environment(
+        date=f"{year}-07-01"
     )
-    def test_renten_alter(
-            self,
-            year: int,
-            test_data: PolicyTestData,
-            column: str,
-    ):
-        df = test_data.input_df
-        policy_params, policy_functions = cached_set_up_policy_environment(
-            date=f"{year}-07-01"
-        )
 
-        calc_result = compute_taxes_and_transfers(
-            data=df,
-            params=policy_params,
-            functions=policy_functions,
-            targets=column,
-        )
-        assert_series_equal(calc_result[column], test_data.output_df[column], atol=1e-1, rtol=0)
+    calc_result = compute_taxes_and_transfers(
+        data=df,
+        params=policy_params,
+        functions=policy_functions,
+        targets=output_columns,
+    )
+    assert_frame_equal(calc_result[output_columns], test_data.output_df, atol=1e-1, rtol=0)
