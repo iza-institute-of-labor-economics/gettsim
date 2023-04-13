bf2706113c0a1a3dcd7b00bfe64fbaec6b62a9e8
LauraGergeleit <93579663+LauraGergeleit@users.noreply.github.com>
2023-04-13 17:06:08 +0200

    Convert test data from csv to yaml (#553)
    Co-authored-by: Lars Reimann <mail@larsreimann.com>

--- src/_gettsim_tests/test_eink_st.py
+++ src/_gettsim_tests/test_eink_st.py
@@ -1,49 +1,29 @@
-import itertools
-
-import pandas as pd
 import pytest
 from _gettsim.interface import compute_taxes_and_transfers
 from pandas.testing import assert_series_equal
 
-from _gettsim_tests import TEST_DATA_DIR
 from _gettsim_tests._helpers import cached_set_up_policy_environment
+from _gettsim_tests._policy_test_utils import PolicyTestData, load_policy_test_data
 
-INPUT_COLS = [
-    "p_id",
-    "hh_id",
-    "tu_id",
-    "kind",
-    "zu_verst_eink_kein_kinderfreib",
-    "zu_verst_eink_kinderfreib",
+OVERRIDE_COLS = [
+    "_zu_verst_eink_ohne_kinderfreib_tu",
+    "zu_verst_eink_mit_kinderfreib_tu",
     "kapitaleink_brutto",
 ]
 
-TEST_COLUMNS = [
-    "eink_st_ohne_kinderfreib_tu",
-    "eink_st_mit_kinderfreib_tu",
-    "abgelt_st_tu",
-    "soli_st_tu",
-]
-YEARS = [2009, 2012, 2015, 2018, 2023]
+data = load_policy_test_data("eink_st")
 
 
-@pytest.fixture(scope="module")
-def input_data():
-    file_name = "eink_st.csv"
-    out = pd.read_csv(TEST_DATA_DIR / file_name)
-    return out
-
-
-@pytest.mark.parametrize("year, column", itertools.product(YEARS, TEST_COLUMNS))
+@pytest.mark.parametrize(
+    ("test_data", "column"),
+    data.parametrize_args,
+    ids=str,
+)
 def test_eink_st(
-    input_data,
-    year,
-    column,
+    test_data: PolicyTestData,
+    column: str,
 ):
-    policy_params, policy_functions = cached_set_up_policy_environment(date=year)
-
-    year_data = input_data[input_data["jahr"] == year].reset_index(drop=True)
-    df = year_data[INPUT_COLS].copy()
+    df = test_data.input_df
 
     df["_zu_verst_eink_ohne_kinderfreib_tu"] = (
         df["zu_verst_eink_kein_kinderfreib"].groupby(df["tu_id"]).transform("sum")
@@ -53,20 +33,18 @@ def test_eink_st(
         df["zu_verst_eink_kinderfreib"].groupby(df["tu_id"]).transform("sum")
     )
 
-    columns = [
-        "_zu_verst_eink_ohne_kinderfreib_tu",
-        "zu_verst_eink_mit_kinderfreib_tu",
-        "kapitaleink_brutto",
-    ]
+    policy_params, policy_functions = cached_set_up_policy_environment(
+        date=test_data.date
+    )
 
     result = compute_taxes_and_transfers(
         data=df,
         params=policy_params,
         functions=policy_functions,
         targets=column,
-        columns_overriding_functions=columns,
+        columns_overriding_functions=OVERRIDE_COLS,
     )
 
     assert_series_equal(
-        result[column], year_data[column], check_dtype=False, atol=1, rtol=0
+        result[column], test_data.output_df[column], check_dtype=False, atol=1, rtol=0
     )
