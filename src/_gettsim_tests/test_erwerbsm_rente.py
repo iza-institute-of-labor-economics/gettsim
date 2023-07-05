b09f4b103be27a186a9c9dd96d1764c8fce9fe04
Stefan Klotz <kl.stefan@gmx.de>
2023-07-05 13:32:02 +0200

    preparations for final test

--- src/_gettsim_tests/test_erwerbsm_rente.py
+++ src/_gettsim_tests/test_erwerbsm_rente.py
@@ -1,57 +1,44 @@
-"""Test for whole Erwerbsminderungsrente
+"""
+Test the Zugangsfaktor for Erwerbsminderungsrente
 (pension for reduced earning capacity)
 
 """
-import itertools
 
-import pandas as pd
 import pytest
 from _gettsim.interface import compute_taxes_and_transfers
 from pandas.testing import assert_series_equal
 
-from _gettsim_tests import TEST_DATA_DIR
 from _gettsim_tests._helpers import cached_set_up_policy_environment
+from _gettsim_tests._policy_test_utils import PolicyTestData, load_policy_test_data
 
-INPUT_COLS = [
-    "p_id",
-    "rentner",
-    "teilw_erwerbsm_rente",
-    "wohnort_ost",
-    "jahr_renteneintr",
-    "alter",
-    "entgeltp",
-    "geburtsjahr",
-    "m_beitragszeit",
-]
-
-OUT_COLS = [
-    "erwerbsm_rente_vor_grundr_m",
-]
-
-YEARS = [2020]
+data = load_policy_test_data("erwerbsm_rente")
 
 
-@pytest.fixture(scope="module")
-def input_data():
-    file_name = "test_erwerbsm_rente.csv"
-    out = pd.read_csv(TEST_DATA_DIR / file_name)
-    return out
-
-
-# Data still missing, will be finished till merging
-@pytest.mark.xfail()
-@pytest.mark.parametrize("year, column", itertools.product(YEARS, OUT_COLS))
-def test_erwerbsm_rente_zugangsfaktor(input_data, year, column):
-    year_data = input_data[input_data["jahr"] == year].reset_index(drop=True)
-    df = year_data[INPUT_COLS].copy()
+@pytest.mark.parametrize(
+    ("test_data", "column"),
+    data.parametrize_args,
+    ids=str,
+)
+def test_erwerbsm_rente(
+    test_data: PolicyTestData,
+    column: str,
+):
+    df = test_data.input_df
     policy_params, policy_functions = cached_set_up_policy_environment(
-        date=f"{year}-01-01"
+        date=test_data.date
     )
 
-    calc_result = compute_taxes_and_transfers(
+    result = compute_taxes_and_transfers(
         data=df,
         params=policy_params,
         functions=policy_functions,
         targets=column,
     )
-    assert_series_equal(calc_result[column], year_data[column], atol=1e-1, rtol=0)
+
+    assert_series_equal(
+        result[column],
+        test_data.output_df[column],
+        check_dtype=False,
+        atol=1e-1,
+        rtol=0,
+    )
