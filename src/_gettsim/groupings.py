ed8c10e9ffee5f7a9f2c3a1eda706ddecab310bb
Lars Reimann <mail@larsreimann.com>
2023-07-11 21:38:01 +0200

    feat: dummy for `bg_id`

--- src/_gettsim/groupings.py
+++ src/_gettsim/groupings.py
@@ -4,8 +4,57 @@ import numpy
 
 
 def create_groupings() -> dict[str, Callable]:
-    return {"st_id": st_id_numpy}
+    return {
+        "bg_id": bg_id_numpy,
+        "st_id": st_id_numpy
+    }
 
+def bg_id_numpy(
+    p_id: numpy.ndarray,
+    hh_id: numpy.ndarray,
+    alter: numpy.ndarray,
+    p_id_ehepartner: numpy.ndarray,
+    p_id_elternteil_1: numpy.ndarray,
+    p_id_elternteil_2: numpy.ndarray,
+    enough_income: numpy.ndarray[bool],
+):
+    # Fast access from p_id to index
+    p_id_to_index = {}
+    for index, current_p_id in enumerate(p_id):
+        p_id_to_index[current_p_id] = index
+
+    # Fast access from p_id to p_ids of children
+    p_id_to_p_ids_children = {}
+    for index, current_p_id in enumerate(p_id):
+        current_p_id_elternteil_1 = p_id_elternteil_1[index]
+        current_p_id_elternteil_2 = p_id_elternteil_2[index]
+
+        if current_p_id_elternteil_1 not in p_id_to_p_ids_children:
+            p_id_to_p_ids_children[current_p_id_elternteil_1] = []
+        p_id_to_p_ids_children[current_p_id_elternteil_1].append(current_p_id)
+
+        if current_p_id_elternteil_2 not in p_id_to_p_ids_children:
+            p_id_to_p_ids_children[current_p_id_elternteil_2] = []
+        p_id_to_p_ids_children[current_p_id_elternteil_2].append(current_p_id)
+
+    p_id_to_bg_id = {}
+    next_bg_id = 0
+
+    for index, current_p_id in enumerate(p_id):
+        current_p_id_ehepartner = p_id_ehepartner[index]
+        current_p_id_children = p_id_to_p_ids_children.get(current_p_id, [])
+        current_alters = alter[index]
+        current_enough_income = enough_income[index]
+
+        # TODO
+        p_id_to_bg_id[current_p_id] = next_bg_id
+
+
+    # Compute result vector
+    result = []
+    for current_p_id in p_id:
+        result.append(p_id_to_bg_id[current_p_id])
+    return numpy.asarray(result)
 
 def st_id_numpy(
     p_id: numpy.ndarray,
@@ -17,7 +66,7 @@ def st_id_numpy(
     next_st_id = 0
     result = []
 
-    for current_p_id, index in enumerate(p_id):
+    for index, current_p_id in enumerate(p_id):
         current_p_id_ehepartner = p_id_ehepartner[index]
         current_gemeinsam_veranlagt = gemeinsam_veranlagt[index]
 
@@ -44,5 +93,4 @@ def st_id_numpy(
         p_id_to_gemeinsam_veranlagt[current_p_id] = current_gemeinsam_veranlagt
         next_st_id += 1
 
-    result = numpy.asarray(result)
-    return result
+    return numpy.asarray(result)
