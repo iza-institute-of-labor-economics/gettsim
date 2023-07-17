f29745a06db9e318d6a634e0677f1336a490f017
Lars Reimann <mail@larsreimann.com>
2023-07-17 21:35:31 +0200

    test: add tests from #509

--- src/_gettsim_tests/test_groupings.py
+++ src/_gettsim_tests/test_groupings.py
@@ -7,7 +7,7 @@ from _gettsim_tests._policy_test_utils import PolicyTestData, load_policy_test_d
 
 OVERRIDE_COLS = []
 
-data = load_policy_test_data("bg_id")
+data = load_policy_test_data("groupings")
 
 
 @pytest.mark.parametrize(
@@ -15,7 +15,7 @@ data = load_policy_test_data("bg_id")
     data.parametrize_args,
     ids=str,
 )
-def test_bg_id(
+def test_groupings(
     test_data: PolicyTestData,
     column: str,
 ):
