d295ed0e620821a81271fd4d3c7b054e80b52513
Lars Reimann <mail@larsreimann.com>
2023-07-25 21:24:16 +0200

    style: fix linter issue

--- src/_gettsim_tests/test_time_conversion.py
+++ src/_gettsim_tests/test_time_conversion.py
@@ -241,7 +241,7 @@ class TestCreateFunctionForTimeUnit:
 # https://github.com/iza-institute-of-labor-economics/gettsim/issues/621
 def test_should_not_create_loops():
     time_conversion_functions = create_time_conversion_functions(
-        {"test_d": lambda test_m: 1}, []
+        {"test_d": lambda test_m: test_m}, []
     )
 
     assert "test_m" not in time_conversion_functions
