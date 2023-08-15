318b74a70f4553ecfc5ecdda451b9d115fc94545
Marvin Immesberger <74215010+MImmesberger@users.noreply.github.com>
2023-08-15 14:49:39 +0200

    Update src/_gettsim/social_insurance_contributions/ges_pflegev.py
    Co-authored-by: Hans-Martin von Gaudecker <hmgaudecker@gmail.com>

--- src/_gettsim/social_insurance_contributions/ges_pflegev.py
+++ src/_gettsim/social_insurance_contributions/ges_pflegev.py
@@ -375,7 +375,7 @@ def ges_pflegev_beitr_selbst_m_zusatz_kinderlos_dummy(
 @dates_active(
     start="1995-01-01", end="2004-03-31", change_name="ges_pflegev_beitr_rente_m"
 )
-def ges_pflegev_beitr_rente_m_ohne_zusatz_fuer_kinderlose_reduz_beitrag(
+def ges_pflegev_beitr_rente_m_reduz_beitrag(
     _ges_krankenv_bemessungsgrundlage_rente_m: float,
     ges_pflegev_beitr_satz: float,
 ) -> float:
