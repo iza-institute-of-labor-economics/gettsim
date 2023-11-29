c1a8ed22e13c2e2285cf56eadd524c607781b21d
Lars Reimann <mail@larsreimann.com>
2023-11-29 09:57:54 +0100

    fix: definition of `fg_id` and `bg_id`

--- src/_gettsim/groupings.py
+++ src/_gettsim/groupings.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from collections.abc import Callable
 
 import numpy
@@ -7,17 +8,39 @@ def create_groupings() -> dict[str, Callable]:
     return {"bg_id": bg_id_numpy, "fg_id": fg_id_numpy, "sn_id": sn_id_numpy}
 
 
-def bg_id_numpy(  # noqa: PLR0913
+def bg_id_numpy(
+    fg_id: numpy.ndarray[int],
+    alter: numpy.ndarray[int],
+    eigener_bedarf_gedeckt: numpy.ndarray[bool],
+):
+    """
+    Compute the ID of the Bedarfsgemeinschaft for each person.
+    """
+    counter = Counter()
+    result = []
+
+    for index, current_fg_id in enumerate(fg_id):
+        current_alter = alter[index]
+        current_eigener_bedarf_gedeckt = eigener_bedarf_gedeckt[index]
+        if current_alter < 25 and current_eigener_bedarf_gedeckt:
+            counter[current_fg_id] += 1
+            result.append(current_fg_id * 100 + counter[current_fg_id])
+        else:
+            result.append(current_fg_id * 100)
+
+    return numpy.asarray(result)
+
+
+def fg_id_numpy(  # noqa: PLR0913
     p_id: numpy.ndarray,
     hh_id: numpy.ndarray,
     alter: numpy.ndarray,
     p_id_einstandspartner: numpy.ndarray,
     p_id_elternteil_1: numpy.ndarray,
     p_id_elternteil_2: numpy.ndarray,
-    eigener_bedarf_gedeckt: numpy.ndarray[bool],
 ):
     """
-    Compute the ID of the Bedarfsgemeinschaft for each person.
+    Compute the ID of the Familiengemeinschaft for each person.
     """
     # Build indexes
     p_id_to_index = {}
@@ -41,70 +64,45 @@ def bg_id_numpy(  # noqa: PLR0913
                 p_id_to_p_ids_children[current_p_id_elternteil_2] = []
             p_id_to_p_ids_children[current_p_id_elternteil_2].append(current_p_id)
 
-    p_id_to_bg_id = {}
-    next_bg_id = 0
+    p_id_to_fg_id = {}
+    next_fg_id = 0
 
     for index, current_p_id in enumerate(p_id):
-        # Already assigned a bg
-        if current_p_id in p_id_to_bg_id:
+        # Already assigned a fg
+        if current_p_id in p_id_to_fg_id:
             continue
 
-        p_id_to_bg_id[current_p_id] = next_bg_id
+        p_id_to_fg_id[current_p_id] = next_fg_id
 
         current_vg_id = hh_id[index]
         current_p_id_einstandspartner = p_id_einstandspartner[index]
         current_p_id_children = p_id_to_p_ids_children.get(current_p_id, [])
 
-        # Assign bg to einstandspartner
+        # Assign fg to einstandspartner
         if current_p_id_einstandspartner >= 0:
-            p_id_to_bg_id[current_p_id_einstandspartner] = next_bg_id
+            p_id_to_fg_id[current_p_id_einstandspartner] = next_fg_id
 
-        # Assign bg to children
+        # Assign fg to children
         for current_p_id_child in current_p_id_children:
             child_index = p_id_to_index[current_p_id_child]
             child_vg_id = hh_id[child_index]
-            child_enough_income = eigener_bedarf_gedeckt[child_index]
             child_alter = alter[child_index]
             child_p_id_children = p_id_to_p_ids_children.get(current_p_id_child, [])
 
             if (
-                child_vg_id == current_vg_id
-                and not child_enough_income
-                # TODO (@hmgaudecker): Add correct conditions for grown up children
-                # https://github.com/iza-institute-of-labor-economics/gettsim/pulls/509
-                and child_alter < 25
-                and len(child_p_id_children) == 0
+                    child_vg_id == current_vg_id
+                    # TODO (@hmgaudecker): Add correct conditions for grown up children
+                    # https://github.com/iza-institute-of-labor-economics/gettsim/pulls/509
+                    and child_alter < 25
+                    and len(child_p_id_children) == 0
             ):
-                p_id_to_bg_id[current_p_id_child] = next_bg_id
+                p_id_to_fg_id[current_p_id_child] = next_fg_id
 
-        next_bg_id += 1
+        next_fg_id += 1
 
     # Compute result vector
-    result = [p_id_to_bg_id[current_p_id] for current_p_id in p_id]
+    result = [p_id_to_fg_id[current_p_id] for current_p_id in p_id]
     return numpy.asarray(result)
-
-
-def fg_id_numpy(  # noqa: PLR0913
-    p_id: numpy.ndarray,
-    hh_id: numpy.ndarray,
-    alter: numpy.ndarray,
-    p_id_einstandspartner: numpy.ndarray,
-    p_id_elternteil_1: numpy.ndarray,
-    p_id_elternteil_2: numpy.ndarray,
-):
-    """
-    Compute the ID of the Familiengemeinschaft for each person.
-    """
-    return bg_id_numpy(
-        p_id=p_id,
-        hh_id=hh_id,
-        alter=alter,
-        p_id_einstandspartner=p_id_einstandspartner,
-        p_id_elternteil_1=p_id_elternteil_1,
-        p_id_elternteil_2=p_id_elternteil_2,
-        eigener_bedarf_gedeckt=numpy.full_like(p_id, False),
-    )
-
 
 def sn_id_numpy(
     p_id: numpy.ndarray,
