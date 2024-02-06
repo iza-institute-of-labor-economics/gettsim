113e555b6714306ca17cb85e120064f532654b70
Marvin Immesberger <immesberger@posteo.de>
2024-02-06 16:14:40 +0100

    Remove explicit Jax types.

--- src/_gettsim/aggregation_jax.py
+++ src/_gettsim/aggregation_jax.py
@@ -88,8 +88,8 @@ def grouped_all(column, group_id):
 
 def sum_values_by_index(
     column: jnp.ndarray,
-    id_col: jnp.ndarray[jnp.int64],
-    p_id_col: jnp.ndarray[jnp.int64],
+    id_col: jnp.ndarray,
+    p_id_col: jnp.ndarray,
 ) -> jnp.ndarray:
     fail_if_dtype_not_numeric_or_boolean(column, agg_func="sum_values_by_index")
     if column.dtype == bool:
