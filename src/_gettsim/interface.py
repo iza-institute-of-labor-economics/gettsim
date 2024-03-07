3d131af239609441986dd655b53c97e0f4ca6331
Marvin Immesberger <immesberger@posteo.de>
2024-03-07 19:57:21 +0100

    Remove Kinderzuschlag, Wohngeld, ALG2 Günstigerprüfung ToDo.

--- src/_gettsim/interface.py
+++ src/_gettsim/interface.py
@@ -241,19 +241,6 @@ def _process_and_check_data(data):
         )
     # Check that group variables are constant within groups
     _fail_if_group_variables_not_constant_within_groups(data)
-
-    # Check that tu_id and hh_id are matching. As long as we have not fixed the
-    # Günstigerprüfung between Kinderzuschlag (calculated on tax unit-level)
-    # and Wohngeld/ALG 2 (calculated on hh level), we do not allow for more than one tax
-    # unit within a household.
-    # TODO (@hmgaudecker): Remove check once groupings allow
-    # for it.
-    # https://github.com/iza-institute-of-labor-economics/gettsim/pull/601
-    if ("tu_id" in data) and ("hh_id" in data):
-        assert (
-            not data["bg_id"].groupby(data["hh_id"]).std().max() > 0
-        ), "We currently allow for only one tax unit within each household"
-
     _fail_if_pid_is_non_unique(data)
     _fail_if_foreign_keys_are_invalid(data)
 
