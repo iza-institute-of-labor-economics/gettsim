44258b2ea16d174fe0e79556ea8700aac34d3283
Marvin Immesberger <immesberger@posteo.de>
2024-03-17 18:16:38 +0100

    Fix docstring.

--- src/_gettsim/transfers/kinderzuschl/kost_unterk.py
+++ src/_gettsim/transfers/kinderzuschl/kost_unterk.py
@@ -72,7 +72,7 @@ def heizkosten_m_bg(
 def _anteil_personen_in_haushalt_bg(
     anz_personen_bg: int, anz_personen_hh: int
 ) -> float:
-    """Calculate the share of persons in this Bedarfsgemeinschaft among all persons in the household.
+    """Share of persons in this Bedarfsgemeinschaft among all persons in the household.
 
     Parameters
     ----------
