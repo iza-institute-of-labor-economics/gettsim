a253307b0b9647699079615bb1d8435ca83ec427
Marvin Immesberger <immesberger@posteo.de>
2024-04-11 14:06:57 +0200

    Update test file to newer standard.

--- src/_gettsim_tests/test_renten_alter.py
+++ src/_gettsim_tests/test_renten_alter.py
@@ -1,44 +1,35 @@
-import itertools
-
 import pytest
 from _gettsim.interface import compute_taxes_and_transfers
-from pandas._testing import assert_series_equal
+from pandas.testing import assert_series_equal
 
 from _gettsim_tests._helpers import cached_set_up_policy_environment
-from _gettsim_tests._policy_test_utils import load_policy_test_data
-
-YEARS = [
-    2014,
-    2015,
-    2016,
-    2017,
-]
+from _gettsim_tests._policy_test_utils import PolicyTestData, load_policy_test_data
 
 data = load_policy_test_data("renten_alter")
-merged_input_df = data.merged_input_df()
-merged_output_df = data.merged_output_df()
-targets = merged_output_df.columns
 
 
 @pytest.mark.parametrize(
-    ("year", "target"),
-    itertools.product(YEARS, targets),
+    ("test_data", "column"),
+    data.parametrize_args,
     ids=str,
 )
-def test_renten_alter(
-    year: int,
-    target: str,
+def test_kindergeld(
+    test_data: PolicyTestData,
+    column: str,
 ):
+    df = test_data.input_df
     policy_params, policy_functions = cached_set_up_policy_environment(
-        date=f"{year}-07-01"
+        date=test_data.date
     )
 
-    calc_result = compute_taxes_and_transfers(
-        data=merged_input_df,
-        params=policy_params,
-        functions=policy_functions,
-        targets=targets,
+    result = compute_taxes_and_transfers(
+        data=df, params=policy_params, functions=policy_functions, targets=column
     )
+
     assert_series_equal(
-        calc_result[target], merged_output_df[target], atol=1e-1, rtol=0
+        result[column],
+        test_data.output_df[column],
+        check_dtype=False,
+        atol=1e-1,
+        rtol=0,
     )
