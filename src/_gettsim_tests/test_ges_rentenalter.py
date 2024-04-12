f35f4fae978ac40ee96b92db6f333b1bb0a6eb1d
Marvin Immesberger <immesberger@posteo.de>
2024-04-12 11:09:44 +0200

    Rename renten_alter to ges_rentenalter.

--- src/_gettsim_tests/test_ges_rentenalter.py
+++ src/_gettsim_tests/test_ges_rentenalter.py
@@ -5,7 +5,7 @@ from pandas.testing import assert_series_equal
 from _gettsim_tests._helpers import cached_set_up_policy_environment
 from _gettsim_tests._policy_test_utils import PolicyTestData, load_policy_test_data
 
-data = load_policy_test_data("renten_alter")
+data = load_policy_test_data("ges_rentenalter")
 
 
 @pytest.mark.parametrize(
