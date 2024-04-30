6bc405d26ddbacc34afcebb8ed33024d9745bc34
Lars Reimann <mail@larsreimann.com>
2024-04-30 17:25:46 +0200

    fix: fall back to default value if foreign key is unresolved

--- src/_gettsim_tests/test_join.py
+++ src/_gettsim_tests/test_join.py
@@ -4,25 +4,42 @@ from _gettsim.shared import join_numpy
 
 
 @pytest.mark.parametrize(
-    "foreign_key, primary_key, target, expected",
+    "foreign_key, primary_key, target, default, expected",
     [
         (
             np.array([1, 2, 3]),
             np.array([1, 2, 3]),
             np.array(["a", "b", "c"]),
+            "d",
             np.array(["a", "b", "c"]),
         ),
         (
             np.array([3, 2, 1]),
             np.array([1, 2, 3]),
             np.array(["a", "b", "c"]),
+            "d",
             np.array(["c", "b", "a"]),
         ),
         (
             np.array([1, 1, 1]),
             np.array([1, 2, 3]),
             np.array(["a", "b", "c"]),
+            "d",
             np.array(["a", "a", "a"]),
+        ),
+        (
+            np.array([-1]),
+            np.array([1]),
+            np.array(["a"]),
+            "d",
+            np.array(["d"]),
+        ),
+        (
+            np.array([2]),
+            np.array([1]),
+            np.array(["a"]),
+            "d",
+            np.array(["d"]),
         ),
     ],
 )
@@ -30,16 +47,20 @@ def test_join_numpy(
     foreign_key: np.ndarray[int],
     primary_key: np.ndarray[int],
     target: np.ndarray[str],
+    default: str,
     expected: np.ndarray[str],
 ):
-    assert np.array_equal(join_numpy(foreign_key, primary_key, target), expected)
+    assert np.array_equal(
+        join_numpy(foreign_key, primary_key, target, default),
+        expected,
+    )
 
 
 def test_join_numpy_raises_duplicate_primary_key():
     with pytest.raises(ValueError, match="Duplicate primary keys:"):
-        join_numpy(np.array([1, 1, 1]), np.array([1, 1, 1]), np.array(["a", "b", "c"]))
-
-
-def test_join_numpy_raises_invalid_foreign_key():
-    with pytest.raises(ValueError, match="Invalid foreign keys:"):
-        join_numpy(np.array([2]), np.array([1]), np.array(["a"]))
+        join_numpy(
+            np.array([1, 1, 1]),
+            np.array([1, 1, 1]),
+            np.array(["a", "b", "c"]),
+            "default",
+        )
