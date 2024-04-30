6c234b299771d771c261747ea450e87f1f269e4b
Lars Reimann <mail@larsreimann.com>
2024-04-30 19:41:03 +0200

    fix: fall back to default value if foreign key of `join_numpy` is unresolved (#741)
    ### What problem do you want to solve?
    
    Closes #740
    
    `join_numpy` now takes a value that is used if a foreign key cannot be
    resolved, e.g. because it is explicitly set to `-1`.
    
    
    ### Todo
    
    - [X] Pick an appropriate title.
    - [X] Put `Closes #XXXX` in the first PR comment to auto-close the
    relevant issue once
    the PR is accepted. This is not applicable if there is no corresponding
    issue.
    - [ ] ~~Document PR in CHANGES.md.~~
    
    ---------
    
    Co-authored-by: Marvin Immesberger <immesberger@posteo.de>

--- src/_gettsim_tests/test_join.py
+++ src/_gettsim_tests/test_join.py
@@ -4,25 +4,35 @@ from _gettsim.shared import join_numpy
 
 
 @pytest.mark.parametrize(
-    "foreign_key, primary_key, target, expected",
+    "foreign_key, primary_key, target, value_if_foreign_key_is_missing, expected",
     [
         (
             np.array([1, 2, 3]),
             np.array([1, 2, 3]),
             np.array(["a", "b", "c"]),
+            "d",
             np.array(["a", "b", "c"]),
         ),
         (
             np.array([3, 2, 1]),
             np.array([1, 2, 3]),
             np.array(["a", "b", "c"]),
+            "d",
             np.array(["c", "b", "a"]),
         ),
         (
             np.array([1, 1, 1]),
             np.array([1, 2, 3]),
             np.array(["a", "b", "c"]),
+            "d",
             np.array(["a", "a", "a"]),
+        ),
+        (
+            np.array([-1]),
+            np.array([1]),
+            np.array(["a"]),
+            "d",
+            np.array(["d"]),
         ),
     ],
 )
@@ -30,16 +40,25 @@ def test_join_numpy(
     foreign_key: np.ndarray[int],
     primary_key: np.ndarray[int],
     target: np.ndarray[str],
+    value_if_foreign_key_is_missing: str,
     expected: np.ndarray[str],
 ):
-    assert np.array_equal(join_numpy(foreign_key, primary_key, target), expected)
+    assert np.array_equal(
+        join_numpy(foreign_key, primary_key, target, value_if_foreign_key_is_missing),
+        expected,
+    )
 
 
 def test_join_numpy_raises_duplicate_primary_key():
     with pytest.raises(ValueError, match="Duplicate primary keys:"):
-        join_numpy(np.array([1, 1, 1]), np.array([1, 1, 1]), np.array(["a", "b", "c"]))
+        join_numpy(
+            np.array([1, 1, 1]),
+            np.array([1, 1, 1]),
+            np.array(["a", "b", "c"]),
+            "default",
+        )
 
 
 def test_join_numpy_raises_invalid_foreign_key():
     with pytest.raises(ValueError, match="Invalid foreign keys:"):
-        join_numpy(np.array([2]), np.array([1]), np.array(["a"]))
+        join_numpy(np.array([2]), np.array([1]), np.array(["a"]), "d")
