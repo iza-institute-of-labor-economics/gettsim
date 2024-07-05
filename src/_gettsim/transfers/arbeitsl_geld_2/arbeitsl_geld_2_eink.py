e0681df39b96dc3dd54e053623d11a51d1f3d176
Marvin Immesberger <immesberger@posteo.de>
2024-07-05 17:52:00 +0200

    Move back to ESt.

--- src/_gettsim/transfers/arbeitsl_geld_2/arbeitsl_geld_2_eink.py
+++ src/_gettsim/transfers/arbeitsl_geld_2/arbeitsl_geld_2_eink.py
@@ -70,15 +70,16 @@ def arbeitsl_geld_2_nettoeink_mit_freibetrag_m(
     return arbeitsl_geld_2_nettoeink_ohne_freibetrag_m - arbeitsl_geld_2_eink_anr_frei_m
 
 
-def arbeitsl_geld_2_nettoeink_ohne_freibetrag_m(
+def arbeitsl_geld_2_nettoeink_ohne_freibetrag_m(  # noqa: PLR0913
     arbeitsl_geld_2_bruttoeink_m: float,
     eink_st_y_sn: float,
     soli_st_y_sn: float,
     anz_personen_sn: int,
     sozialv_beitr_arbeitnehmer_m: float,
+    arbeitsl_geld_2_eink_anr_frei_m: float,
 ) -> float:
-    """Net income for calculation of basic subsistence (Arbeitslosengeld II /
-    Bürgergeld).
+    """Income (after deduction of taxes, social insurance contributions, and other
+    deductions) for calculation of basic subsistence.
 
     Note: Since 2023, Arbeitslosengeld 2 is referred to as Bürgergeld.
 
@@ -94,6 +95,14 @@ def arbeitsl_geld_2_nettoeink_ohne_freibetrag_m(
         See :func:`anz_personen_sn`.
     sozialv_beitr_arbeitnehmer_m
         See :func:`sozialv_beitr_arbeitnehmer_m`.
+    eink_st_y_sn
+        See :func:`eink_st_y_sn`.
+    soli_st_y_sn
+        See :func:`soli_st_y_sn`.
+    anz_personen_sn
+        See :func:`anz_personen_sn`.
+    arbeitsl_geld_2_eink_anr_frei_m
+        See :func:`arbeitsl_geld_2_eink_anr_frei_m`.
 
     Returns
     -------
@@ -105,6 +114,7 @@ def arbeitsl_geld_2_nettoeink_ohne_freibetrag_m(
         - (eink_st_y_sn / 12 / anz_personen_sn)
         - (soli_st_y_sn / 12 / anz_personen_sn)
         - sozialv_beitr_arbeitnehmer_m
+        - arbeitsl_geld_2_eink_anr_frei_m
     )
 
 
