4ba9225d6efe52c3e50f60e025da2f6144096e26
Marvin Immesberger <immesberger@posteo.de>
2024-07-07 13:34:48 +0200

    Add test file, change folder structure.

---
+++ src/_gettsim_tests/test_endogenous_bg_id.py
@@ -0,0 +1,32 @@
+import pytest
+from _gettsim.interface import compute_taxes_and_transfers
+from pandas.testing import assert_series_equal
+
+from _gettsim_tests._helpers import cached_set_up_policy_environment
+from _gettsim_tests._policy_test_utils import PolicyTestData, load_policy_test_data
+
+data = load_policy_test_data("endogenous_bg_id")
+
+
+@pytest.mark.parametrize(
+    ("test_data", "column"),
+    data.parametrize_args,
+    ids=str,
+)
+def test_endogenous_bg_id(
+    test_data: PolicyTestData,
+    column: str,
+):
+    df = test_data.input_df
+
+    policy_params, policy_functions = cached_set_up_policy_environment(
+        date=test_data.date
+    )
+
+    result = compute_taxes_and_transfers(
+        data=df, params=policy_params, functions=policy_functions, targets=column
+    )
+
+    assert_series_equal(
+        result[column], test_data.output_df[column], check_dtype=False, atol=1, rtol=0
+    )
