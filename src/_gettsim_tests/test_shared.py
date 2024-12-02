8b763eed8fa8e9a961779a693b826806114e5b35
Marvin Immesberger <immesberger@posteo.de>
2024-12-02 11:54:15 +0100

    Remove leftover test.

--- src/_gettsim_tests/test_shared.py
+++ src/_gettsim_tests/test_shared.py
@@ -1,40 +1,9 @@
-import pandas as pd
 import pytest
 
 from _gettsim.shared import (
     create_dict_from_list,
     merge_nested_dicts,
-    parse_input_to_nested_dict,
 )
-
-
-@pytest.mark.parametrize(
-    "function_input, separator, expected",
-    [
-        ("a", "__", {"a": None}),
-        ("a__b", "__", {"a": {"b": None}}),
-        (["a", "b"], "__", {"a": None, "b": None}),
-        (["a__b", "c__d"], "__", {"a": {"b": None}, "c": {"d": None}}),
-        (["a__b", "a__c"], "__", {"a": {"b": None, "c": None}}),
-        (["a.b", "a.c", "a.b.d"], ".", {"a": {"b": {"d": None}, "c": None}}),
-        ({"a": "b"}, "__", {"a": "b"}),
-    ],
-)
-def test_parse_input_to_nested_dict(function_input, separator, expected):
-    assert parse_input_to_nested_dict(function_input, separator, name="_") == expected
-
-
-@pytest.mark.parametrize(
-    "function_input",
-    [
-        (pd.DataFrame()),
-        (pd.Series()),
-        (1),
-    ],
-)
-def test_parse_input_to_nested_dict_type_error(function_input):
-    with pytest.raises(NotImplementedError):
-        parse_input_to_nested_dict(function_input, separator=".", name="_")
 
 
 @pytest.mark.parametrize(
