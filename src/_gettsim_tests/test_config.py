1bfdbc007b55e4161f165bf3bb05ce7f75b5d2c5
timmens <mensingertim@gmail.com>
2025-01-21 18:56:04 +0100

    Fix test_config.py

--- src/_gettsim_tests/test_config.py
+++ src/_gettsim_tests/test_config.py
@@ -1,37 +1,22 @@
-import importlib
-
 import pytest
 
 import _gettsim
 
 
-def test_default_backend():
+def test_conftest_set_array_backend_updates_use_jax(request):
+    expected = request.config.option.USE_JAX
+    from _gettsim.config import USE_JAX
+
+    assert expected == USE_JAX
+
+
+def test_conftest_set_array_backend_updates_backend(request):
+    use_jax = request.config.option.USE_JAX
+    expected = "jax.numpy" if use_jax else "numpy"
     from _gettsim.config import numpy_or_jax
 
-    assert numpy_or_jax.__name__ == "numpy"
-
-
-def test_set_backend():
-    is_jax_installed = importlib.util.find_spec("jax") is not None
-
-    # expect default backend
-    from _gettsim.config import numpy_or_jax
-
-    assert numpy_or_jax.__name__ == "numpy"
-
-    if is_jax_installed:
-        # set jax backend
-        _gettsim.config.set_array_backend("jax")
-        from _gettsim.config import numpy_or_jax
-
-        assert numpy_or_jax.__name__ == "jax.numpy"
-
-        from _gettsim.config import USE_JAX
-
-        assert USE_JAX
-    else:
-        with pytest.raises(AssertionError):
-            _gettsim.config.set_array_backend("jax")
+    got = numpy_or_jax.__name__
+    assert expected == got
 
 
 @pytest.mark.parametrize("backend", ["dask", "jax.numpy"])
