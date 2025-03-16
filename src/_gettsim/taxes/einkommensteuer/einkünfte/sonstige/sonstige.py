7520b4028fc0ed6797c07dfb58a9fd6dc5745735
Marvin Immesberger <immesberger@posteo.de>
2025-03-16 14:54:34 +0100

    Forogt to commit this.

--- src/_gettsim/taxes/einkommensteuer/einkünfte/sonstige/sonstige.py
+++ src/_gettsim/taxes/einkommensteuer/einkünfte/sonstige/sonstige.py
@@ -5,6 +5,6 @@ from _gettsim.function_types import policy_function
 
 @policy_function()
 def betrag_y_sn(
-    einnahmen__sonstige_einkünfte_y: float,
+    einnahmen__sonstige_m: float,
 ) -> float:
     raise NotImplementedError("Not implemented yet.")
