48f5d6f5ffa51f2f75574614472d97e1f7156700
Marvin Immesberger <immesberger@posteo.de>
2025-03-20 00:54:39 +0100

    Rename all tests dirs.

---
+++ src/_gettsim_tests/test_kinderzuschlag.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING
+
+import dags.tree as dt
+import pytest
+from numpy.testing import assert_array_almost_equal
+
+from _gettsim.interface import compute_taxes_and_transfers
+from _gettsim_tests._helpers import cached_set_up_policy_environment
+from _gettsim_tests._policy_test_utils import PolicyTest, load_policy_test_data
+
+if TYPE_CHECKING:
+    import datetime
+
+    from _gettsim.gettsim_typing import NestedDataDict, NestedInputStructureDict
+
+
+test_data = load_policy_test_data("kinderzuschlag")
+
+
+@pytest.mark.parametrize(
+    "test",
+    test_data,
+)
+def test_kinderzuschlag(
+    test: PolicyTest,
+):
+    date: datetime.date = test.date
+    input_tree: NestedDataDict = test.input_tree
+    expected_output_tree: NestedDataDict = test.expected_output_tree
+    target_structure: NestedInputStructureDict = test.target_structure
+
+    environment = cached_set_up_policy_environment(date=date)
+
+    result = compute_taxes_and_transfers(
+        data_tree=input_tree, environment=environment, targets_tree=target_structure
+    )
+
+    flat_result = dt.flatten_to_qual_names(result)
+    flat_expected_output_tree = dt.flatten_to_qual_names(expected_output_tree)
+
+    for result, expected in zip(
+        flat_result.values(), flat_expected_output_tree.values()
+    ):
+        assert_array_almost_equal(result, expected, decimal=2)
---
+++ src/_gettsim_tests/test_lohnsteuer.py
@@ -0,0 +1,60 @@
+from typing import TYPE_CHECKING
+
+import dags.tree as dt
+import pytest
+from numpy.testing import assert_array_almost_equal
+
+from _gettsim.interface import compute_taxes_and_transfers
+from _gettsim.policy_environment import set_up_policy_environment
+from _gettsim_tests._helpers import cached_set_up_policy_environment
+from _gettsim_tests._policy_test_utils import PolicyTest, load_policy_test_data
+
+if TYPE_CHECKING:
+    import datetime
+
+    from _gettsim.gettsim_typing import NestedDataDict, NestedInputStructureDict
+
+
+test_data = load_policy_test_data("lohnsteuer")
+
+
+@pytest.mark.parametrize(
+    "test",
+    test_data,
+)
+def test_lohnsteuer(
+    test: PolicyTest,
+):
+    date: datetime.date = test.date
+    input_tree: NestedDataDict = test.input_tree
+    expected_output_tree: NestedDataDict = test.expected_output_tree
+    target_structure: NestedInputStructureDict = test.target_structure
+
+    environment = cached_set_up_policy_environment(date=date)
+
+    result = compute_taxes_and_transfers(
+        data_tree=input_tree, environment=environment, targets_tree=target_structure
+    )
+
+    flat_result = dt.flatten_to_qual_names(result)
+    flat_expected_output_tree = dt.flatten_to_qual_names(expected_output_tree)
+
+    for result, expected in zip(
+        flat_result.values(), flat_expected_output_tree.values()
+    ):
+        assert_array_almost_equal(result, expected, decimal=0)
+
+
+def test_lohnsteuer_rv_anteil():
+    environment = set_up_policy_environment(2018)
+
+    assert (
+        environment.params["eink_st_abzuege"]["vorsorgepauschale_rentenv_anteil"]
+        == 0.72
+    )
+
+    environment = set_up_policy_environment(2023)
+
+    assert (
+        environment.params["eink_st_abzuege"]["vorsorgepauschale_rentenv_anteil"] == 1
+    )
